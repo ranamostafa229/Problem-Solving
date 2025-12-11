1e8bfb15075f6f9e703cf544d867ca87ef8dd8ad
Your Name <ranamostafa2210@gmail.com>
2025-12-11 23:54:40 +0200

    Solve Who ate the cookie? problem

--- kata8.js
+++ kata8.js
@@ -76,4 +76,26 @@ All values will be positive integers or floats, or zero.
 function plural(n) {
   return n !== 1;
 }
-console.log(plural(Infinity)); // true
+// console.log(plural(Infinity)); // true
+
+/* (5) Who ate the cookie?
+For this problem you must create a program that says who ate the last cookie. 
+If the input is a string then "Zach" ate the cookie. 
+If the input is a float or an int then "Monica" ate the cookie. 
+If the input is anything else "the dog" ate the cookie. 
+The way to return the statement is: "Who ate the last cookie? It was (name)!"
+
+Ex: Input = "hi" --> Output = "Who ate the last cookie? It was Zach! (The reason you return Zach is because the input is a string)
+*/
+
+function cookie(x) {
+  return (
+    `Who ate the last cookie? It was ` +
+    (typeof x === "string"
+      ? "Zach!"
+      : typeof x === "number"
+      ? "Monica!"
+      : "the dog!")
+  );
+}
+console.log(cookie(2.3));
