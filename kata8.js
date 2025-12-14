13c605e2d3e81d70c929ef3a3623bb2d7e0fd84b
Your Name <ranamostafa2210@gmail.com>
2025-12-14 23:43:01 +0200

    Solve The falling speed of petals problem

--- kata8.js
+++ kata8.js
@@ -160,4 +160,28 @@ function generateShape(integer) {
   }
   return output.join("");
 }
-console.log(generateShape(8));
+// console.log(generateShape(8));
+
+/* (8) The falling speed of petals
+When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. 
+The petals start to fall in late April.
+Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), 
+and it takes 80 seconds for the petal to reach the ground from a certain branch.
+
+Write a function that receives the speed (in cm/s) of a petal as input, 
+and returns the time it takes for that petal to reach the ground from the same branch.
+
+Notes:
+
+The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
+Pay attention to the data types.
+If the initial velocity is non-positive, the return value should be 0
+
+*/
+
+// v= distance / time 5= d/80 => d=400cm
+
+function sakuraFall(v) {
+  return v <= 0 ? 0 : 400 / v;
+}
+console.log(sakuraFall(200));
