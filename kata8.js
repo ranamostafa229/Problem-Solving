a949ae0e193f7f3b2dfb5cc42e92ce4365188ef6
Your Name <ranamostafa2210@gmail.com>
2025-12-18 18:39:42 +0200

    Solve Remove First and Last Character Part Two problem

--- kata8.js
+++ kata8.js
@@ -277,4 +277,40 @@ function digitize(n) {
     .map((i) => Number(i));
 }
 
-console.log(digitize(35231));
+// console.log(digitize(35231));
+
+/* (14) Remove First and Last Character Part Two
+You are given a string containing a sequence of character sequences separated by commas.
+Write a function which returns a new string containing the same character sequences except the first and the last ones but this time separated by spaces.
+If the input string is empty or the removal of the first and last items would cause 
+the resulting string to be empty, return an empty value 
+(represented as a generic value NULL in the examples below).
+
+Examples
+"1,2,3"      =>  "2"
+"1,2,3,4"    =>  "2 3"
+"1,2,3,4,5"  =>  "2 3 4"
+
+""     =>  NULL
+"1"    =>  NULL
+"1,2"  =>  NULL
+*/
+
+// input => a sequence of character sequences separated by commas => string
+// output => a new string containing the same character sequences except the first and the last ones
+//  but this time separated by spaces. => string or null
+function array(string) {
+  const stringArray = string.split(",");
+  return stringArray.length < 3
+    ? null
+    : stringArray
+        .join(" ")
+        .slice(
+          stringArray.join(",").indexOf(","),
+          stringArray.join(",").lastIndexOf(",")
+        )
+        .trim();
+  // [OR]
+  // return string.split(",").slice(1, -1).join(" ") || null;
+}
+console.log(array("A1,B2,C3,D4,E5"));
