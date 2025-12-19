f38828327a879fc3cd584d0ba713d508d313f349
Your Name <ranamostafa2210@gmail.com>
2025-12-19 17:30:15 +0200

    Solve Geometry Basics: Distance between points in 2D problem

--- kata8.js
+++ kata8.js
@@ -327,4 +327,21 @@ Constraint:
 const quarterOf = (month) => {
   return Math.ceil(month / 3);
 };
-console.log(quarterOf(11));
+// console.log(quarterOf(11));
+
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+Point objects have attributes x and y.
+Write a function calculating distance between Point a and Point b.
+
+Input coordinates fit in range 
+−50⩽x,
+y⩽50
+Tests compare expected result and actual answer with tolerance of 1e-6.
+*/
+function distanceBetweenPoints(a, b) {
+  return Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2);
+  // [OR] Math.hypot() calculate the square root of the sum of squares of its arguments
+  // return Math.hypot(b.x - a.x, b.y - a.y);
+}
+console.log(distanceBetweenPoints({ x: 1, y: 1 }, { x: 2, y: 2 }));
