3201ba2f7e82d7bd0a4ec87632ff2c9aa2a60c7b
Your Name <ranamostafa2210@gmail.com>
2025-12-19 17:14:29 +0200

    Solve Quarter of the year problem

--- kata8.js
+++ kata8.js
@@ -313,4 +313,18 @@ function array(string) {
   // [OR]
   // return string.split(",").slice(1, -1).join(" ") || null;
 }
-console.log(array("A1,B2,C3,D4,E5"));
+// console.log(array("A1,B2,C3,D4,E5"));
+
+/* (15) Quarter of the year
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+For example: month 2 (February), is part of the first quarter; 
+month 6 (June), is part of the second quarter; 
+and month 11 (November), is part of the fourth quarter.
+
+Constraint:
+1 <= month <= 12
+*/
+const quarterOf = (month) => {
+  return Math.ceil(month / 3);
+};
+console.log(quarterOf(11));
