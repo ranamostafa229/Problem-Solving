1aa8fe8b09a99e5e84a7397da04c91d13aa8ff2c
Your Name <ranamostafa2210@gmail.com>
2025-12-22 22:47:30 +0200

    Solve Do you speak "English"? problem

--- kata8.js
+++ kata8.js
@@ -408,4 +408,22 @@ For example(Input --> Output):
 function howManyLightsabersDoYouOwn(name) {
   return name === "Zach" ? 18 : 0;
 }
-console.log(howManyLightsabersDoYouOwn("Zach")); //18
+// console.log(howManyLightsabersDoYouOwn("Zach")); //18
+
+/*
+Given a string of arbitrary length with any ascii characters. 
+Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct
+ but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+Return value as boolean values, true for the string to contains "English", false for it does not.
+*/
+
+function spEng(sentence) {
+  return sentence.match(/english/i) ? true : false;
+}
+console.log(spEng("english")); // true
+console.log(spEng("abcEnglishdef")); // true
+console.log(spEng("abcnEglishsef")); // false
