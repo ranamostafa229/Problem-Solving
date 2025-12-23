c39026fbec797d1191b08647eeacc93a48a8687f
Your Name <ranamostafa2210@gmail.com>
2025-12-23 22:35:53 +0200

    Solve A Needle in the Haystack problem

--- kata8.js
+++ kata8.js
@@ -453,5 +453,44 @@ function validateHello(greetings) {
   res = /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings);
   return res;
 }
-console.log(validateHello("meh"));
-console.log(validateHello("hallo"));
+// console.log(validateHello("meh")); // false
+// console.log(validateHello("hallo")); // true
+// console.log(validateHello("ciao bella!")); // true
+
+/* (24) A Needle in the Haystack
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+After your function finds the needle it should return a message (as a string) that says:
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+
+*/
+
+function findNeedle(haystack) {
+  return (
+    "found the needle at position " +
+    haystack.findIndex((item) => item === "needle")
+  );
+  // [OR]
+  // return "found the needle at position " + haystack.indexOf("needle");
+}
+console.log(
+  findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])
+); // "found the needle at position 5"
+console.log(
+  findNeedle([
+    "3",
+    "123124234",
+    undefined,
+    "needle",
+    "world",
+    "hay",
+    2,
+    "3",
+    true,
+    false,
+  ])
+);
