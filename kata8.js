7e988e4b7113ffc5f2a1298053f1a2d7fb04b6d0
Your Name <ranamostafa2210@gmail.com>
2025-12-23 23:21:58 +0200

    Solve How good are you really? problem

--- kata8.js
+++ kata8.js
@@ -513,4 +513,28 @@ function unusualFive() {
   return "five!".length;
 }
 
-console.log(unusualFive()); // 5
+// console.log(unusualFive()); // 5
+
+/* (27) How good are you really?
+There was a test in your class and you passed it. Congratulations!
+
+But you're an ambitious person. You want to know if you're better than 
+the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+Return true if you're better, else false!
+
+Note:
+Your points are not included in the array of your class's points. 
+Do not forget them when calculating the average score!
+*/
+function betterThanAverage(classPoints, yourPoints) {
+  return classPoints.reduce((acc, curr) => acc + curr, yourPoints) /
+    (classPoints.length + 1) >
+    yourPoints
+    ? false
+    : true;
+}
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)); // true
+console.log(betterThanAverage([50, 50, 50], 50)); // false
+console.log(betterThanAverage([2, 3], 5)); // true
