130ca503db83dbdcc34a83dc96e113b1cfbc2634
Your Name <ranamostafa2210@gmail.com>
2025-12-24 23:51:36 +0200

    Solve  Find the first non-consecutive number problem

--- kata8.js
+++ kata8.js
@@ -578,6 +578,35 @@ function whoIsPaying(name) {
   // [OR]
   // return name.length <= 2 ? [name] : [name, name.slice(0, 2)];
 }
-console.log(whoIsPaying("Mexico")); // ["Mexico", "Me"]
-console.log(whoIsPaying("Me")); // ["Me"]
-console.log(whoIsPaying("I")); // ["I"]
+// console.log(whoIsPaying("Mexico")); // ["Mexico", "Me"]
+// console.log(whoIsPaying("Me")); // ["Me"]
+// console.log(whoIsPaying("I")); // ["I"]
+
+/* (30) Find the first non-consecutive number
+Your task is to find the first element of an array that is not consecutive.
+By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, 
+so that's the first non-consecutive number.
+
+If the whole array is consecutive then return null2.
+The array will always have at least 2 elements1 and all elements will be numbers. 
+The numbers will also all be unique and in ascending order. 
+The numbers could be positive or negative and the first non-consecutive could be either too!
+
+1 Can you write a solution that will return null2 for both [] and [ x ] though? 
+(This is an empty array and one with a single number and is not tested for, 
+but you can write your own example test. )
+
+*/
+
+function firstNonConsecutive(arr) {
+  return (
+    arr[arr.findIndex((num, index) => num + 1 !== arr[index + 1]) + 1] ?? null
+  );
+  // ?? =>  only treat null and undefined as false
+  // || would treat other falsy values like 0 as false (0, "", false, NaN) so it is not suitable here
+}
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])); // 6
+console.log(firstNonConsecutive([1, 2, 3, 4])); // null
+console.log(firstNonConsecutive([1, 2, 0, 4])); // 0
