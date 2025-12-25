363e7cae734626b2b32bf8b5a9fa8c9e73a82553
Your Name <ranamostafa2210@gmail.com>
2025-12-25 22:20:01 +0200

    Solve Area or Perimeter problem

--- kata8.js
+++ kata8.js
@@ -632,4 +632,23 @@ Examples (Input ==> Output):
 function stringToArray(string) {
   return string.split(" ");
 }
-console.log(stringToArray("I love arrays they are my favorite")); // ["I", "love", "arrays", "they", "are", "my", "favorite"]
+// console.log(stringToArray("I love arrays they are my favorite")); // ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+/* (33) Area or Perimeter
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will assume that it is a square 
+if its length and width are equal, otherwise it is a rectangle.
+
+*/
+const areaOrPerimeter = function (l, w) {
+  return l === w ? l * w : 2 * (w + l);
+};
+
+console.log(areaOrPerimeter(6, 10)); //32
+console.log(areaOrPerimeter(3, 3)); //9
