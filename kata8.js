85349e02843ed507e9467753d9eb9c47fb54f008
Your Name <ranamostafa2210@gmail.com>
2025-12-26 22:44:52 +0200

    Solve Switch it Up! problem

--- kata8.js
+++ kata8.js
@@ -715,5 +715,55 @@ Examples:
 function twoHighest(arr) {
   return [...new Set(arr)].sort((a, b) => b - a).slice(0, 2);
 }
-console.log(twoHighest([15, 20, 20, 17])); // [20, 17]
-console.log(twoHighest([])); // []
+// console.log(twoHighest([15, 20, 20, 17])); // [20, 17]
+// console.log(twoHighest([])); // []
+
+/* (37) Switch it Up!
+When provided with a number between 0-9, return it in words. Note that the input is guaranteed
+ to be within the range of 0-9.
+
+Input: 1
+Output: "One".
+
+If your language supports it, try using a switch statement.
+*/
+function switchItUp(number) {
+  const numberWords = [
+    { 0: "Zero" },
+    { 1: "One" },
+    { 2: "Two" },
+    { 3: "Three" },
+    { 4: "Four" },
+    { 5: "Five" },
+    { 6: "Six" },
+    { 7: "Seven" },
+    { 8: "Eight" },
+    { 9: "Nine" },
+  ];
+  return numberWords[number][number];
+  // [OR] Using switch statement
+  // switch (number) {
+  //   case 0:
+  //     return "Zero";
+  //   case 1:
+  //     return "One";
+  //   case 2:
+  //     return "Two";
+  //   case 3:
+  //     return "Three";
+  //   case 4:
+  //     return "Four";
+  //   case 5:
+  //     return "Five";
+  //   case 6:
+  //     return "Six";
+  //   case 7:
+  //     return "Seven";
+  //   case 8:
+  //     return "Eight";
+  //   case 9:
+  //     return "Nine";
+  // }
+}
+console.log(switchItUp(1)); // "One"
+console.log(switchItUp(5)); // "Five"
