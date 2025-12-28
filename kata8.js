f6649d41e73968a86f0bcc6240b4b09ab37ecfcc
Your Name <ranamostafa2210@gmail.com>
2025-12-28 22:49:31 +0200

    Solve Generate range of integers problem

--- kata8.js
+++ kata8.js
@@ -908,4 +908,30 @@ with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 function stringClean(s) {
   return s.replace(/[0-9]/g, "");
 }
-console.log(stringClean("(E3at m2e2!!)")); // "(Eat me!!)"
+// console.log(stringClean("(E3at m2e2!!)")); // "(Eat me!!)"
+
+/* (45) Generate range of integers
+Implement the function generateRange which takes three arguments (start, stop, step) and returns the range of integers from start to stop (inclusive) in increments of step.
+
+Examples
+(1, 10, 1) -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+(-10, 1, 1) -> [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1]
+(1, 15, 20) -> [1]
+Note
+start < stop
+step > 0
+*/
+function generateRange(min, max, step) {
+  return new Array(Math.floor((max - min) / step + 1))
+    .fill(1)
+    .map((_, i) => min + i * step); // 0> 2 + 0 * 2 = 2+0 = 2 , 1> 2 + 1 * 2 = 2+2 = 4
+  // [OR]
+  // let output = [];
+  // for (let i = min; i <= max; i += step) {
+  //   output.push(i);
+  // }
+  // return output;
+}
+console.log(generateRange(2, 10, 2)); // [2,4,6,8,10]
+console.log(generateRange(1, 10, 1)); // [1,2,3,4,5,6,7,8,9,10]
+console.log(generateRange(1, 10, 4)); // [1,5,9]
