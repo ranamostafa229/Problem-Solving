fb46d6eafc5cd3b30fcd96f1aa945333f8b452d6
Your Name <ranamostafa2210@gmail.com>
2025-12-30 23:16:08 +0200

    Solve Add Length problem

--- kata8.js
+++ kata8.js
@@ -1008,7 +1008,27 @@ function defineSuit(card) {
     "♠": "spades",
   }[card.slice(-1)];
 }
-console.log(defineSuit("3♣")); // 'clubs'
-console.log(defineSuit("3♦")); // 'diamonds'
-console.log(defineSuit("3♥")); // 'hearts'
-console.log(defineSuit("3♠")); // 'spades'
+// console.log(defineSuit("3♣")); // 'clubs'
+// console.log(defineSuit("3♦")); // 'diamonds'
+// console.log(defineSuit("3♥")); // 'hearts'
+// console.log(defineSuit("3♠")); // 'spades'
+
+/* (50) Add Length
+What if we need the length of the words separated by a space to be added at the end of that same word
+and have it returned as an array?
+
+Example(Input --> Output)
+
+"apple ban" --> ["apple 5", "ban 3"]
+"you will win" -->["you 3", "will 4", "win 3"]
+
+Your task is to write a function that takes a String and returns an Array/list with 
+the length of each word added to each element .
+
+Note: String will have at least one element; words will always be separated by a space.
+*/
+function addLength(str) {
+  return str.split(" ").map((word) => word + " " + word.length);
+}
+console.log(addLength("apple ban")); // ["apple 5", "ban 3"]
+console.log(addLength("you will win")); //["you 3", "will 4", "win 3"]
