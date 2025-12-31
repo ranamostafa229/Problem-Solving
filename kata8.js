18b216b3f86b16b9e7fbc4585a7961bb1795179b
Your Name <ranamostafa2210@gmail.com>
2025-12-31 22:24:39 +0200

    Solve Multiple of index problem

--- kata8.js
+++ kata8.js
@@ -1030,5 +1030,22 @@ Note: String will have at least one element; words will always be separated by a
 function addLength(str) {
   return str.split(" ").map((word) => word + " " + word.length);
 }
-console.log(addLength("apple ban")); // ["apple 5", "ban 3"]
-console.log(addLength("you will win")); //["you 3", "will 4", "win 3"]
+// console.log(addLength("apple ban")); // ["apple 5", "ban 3"]
+// console.log(addLength("you will win")); //["you 3", "will 4", "win 3"]
+
+/* (51) Multiple of index
+Return a new array consisting of elements which are multiple of their own index 
+in input array (length > 1).
+
+Some cases:
+[22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
+
+[68, -1, 1, -7, 10, 10] => [-1, 10]
+
+[-56,-85,72,-26,-14,76,-27,72,35,-21,-67,87,0,21,59,27,-92,68] => [-85, 72, 0, 68]
+*/
+function multipleOfIndex(array) {
+  return array.filter((num, index) => num % index === 0 || num === 0);
+}
+console.log(multipleOfIndex([22, -6, 32, 82, 9, 25])); // [-6, 32, 25]
+console.log(multipleOfIndex([0, 2, 3, 6, 9])); // [0, 2,6]
