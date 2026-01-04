cd5634e73cd41d5e6a41f04177133840c4ecced6
Your Name <ranamostafa2210@gmail.com>
2026-01-04 17:49:47 +0200

    Solve Logical calculator problem

--- kata8.js
+++ kata8.js
@@ -1275,7 +1275,47 @@ const players = ["a", "b", "c", "d", "e", "f"].map((name) => new Player(name));
 function duckDuckGoose(players, goose) {
   return players[(goose - 1) % players.length].name;
 }
-console.log(duckDuckGoose(players, 1)); // a
-console.log(duckDuckGoose(players, 3)); // c
-console.log(duckDuckGoose(players, 6)); // f
-console.log(duckDuckGoose(players, 8)); // b
+// console.log(duckDuckGoose(players, 1)); // a
+// console.log(duckDuckGoose(players, 3)); // c
+// console.log(duckDuckGoose(players, 6)); // f
+// console.log(duckDuckGoose(players, 8)); // b
+
+/* (64) Logical calculator
+Given an array of Boolean values and a logical operator, return a Boolean result based on 
+sequentially applying the operator to the values in the array.
+
+Examples
+booleans = [True, True, False], operator = "AND"
+True AND True -> True
+True AND False -> False
+return False
+
+booleans = [True, True, False], operator = "OR"
+True OR True -> True
+True OR False -> True
+return True
+
+booleans = [True, True, False], operator = "XOR"
+True XOR True -> False
+False XOR False -> False
+return False
+
+Input
+an array of Boolean values (1 <= array_length <= 50)
+a string specifying a logical operator: "AND", "OR", "XOR"
+
+Output
+A Boolean value (True or False).
+
+*/
+function logicalCalc(array, op) {
+  const operator = {
+    AND: (a, b) => a && b,
+    OR: (a, b) => a || b,
+    XOR: (a, b) => (a ^ b ? true : false),
+  };
+  return array.reduce((acc, cur) => operator[op](acc, cur));
+}
+console.log(logicalCalc([true, true, true, false], "AND")); // false
+console.log(logicalCalc([true, true, true, false], "OR")); // true
+console.log(logicalCalc([true, true, true, false], "XOR")); // true
