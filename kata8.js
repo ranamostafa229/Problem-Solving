85b3c0e1402d8aba744db919a9b06c79126d9291
Your Name <ranamostafa2210@gmail.com>
2026-01-05 17:22:23 +0200

    Solve Take an Arrow to the knee, Functionally problem

--- kata8.js
+++ kata8.js
@@ -1336,5 +1336,26 @@ Input -> Output
 function distinct(a) {
   return [...new Set(a)];
 }
-console.log(distinct([1, 1, 2])); // [1,2]
-console.log(distinct([1, 2, 1, 1, 3, 2])); // [1,2,3]
+// console.log(distinct([1, 1, 2])); // [1,2]
+// console.log(distinct([1, 2, 1, 1, 3, 2])); // [1,2,3]
+
+/* (66) Take an Arrow to the knee, Functionally
+Arrow style Functions
+Come here to practice the Arrow style functions Not much else to say good luck!
+Details
+You will be given an array of numbers which can be used using the String.fromCharCode() (JS), 
+Tools.FromCharCode() (C#) method to convert the number to a character. 
+It is recommended to map over the array of numbers and convert each number 
+to the corresponding ascii character.
+
+Examples
+These are example of how to convert a number to an ascii Character:
+Javascript => String.fromCharCode(97) // a
+C# => Tools.FromCharCode(97) // a
+*/
+const arrowFunc = function (arr) {
+  return arr.map((num) => String.fromCharCode(num)).join("");
+  // [OR]
+  // return String.fromCharCode(...arr);
+};
+console.log(arrowFunc([84, 101, 115, 116])); // Test
