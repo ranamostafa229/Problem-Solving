ac79c0ffe16eb68857e154f92e1d6ac08c43e878
Your Name <ranamostafa2210@gmail.com>
2026-01-05 17:39:30 +0200

    Solve Grasshopper - Array Mean problem

--- kata8.js
+++ kata8.js
@@ -1434,4 +1434,34 @@ const sunday = [
     7960, 9455, 5633, 9085,
   ];
 const stairs = [sunday, monday, tuesday, wednesday, thursday, friday, saturday];
-console.log(stairsIn20(stairs)); //54636040
+// console.log(stairsIn20(stairs)); //54636040
+
+/* (68) Grasshopper - Array Mean
+Find Mean
+Find the mean (average) of a list of numbers in an array.
+
+Information
+To find the mean (average) of a set of numbers add all of the numbers together 
+and divide by the number of values in the list.
+
+For an example list of 1, 3, 5, 7
+
+1. Add all of the numbers
+
+1+3+5+7 = 16
+2. Divide by the number of values in the list. In this example there are 4 numbers in the list.
+
+16/4 = 4
+3. The mean (or average) of this list is 4
+*/
+function findAverage(nums) {
+  return nums.reduce((acc, cur) => acc + cur, 0) / nums.length;
+  // [OR]
+  // let total = 0;
+  // for (let i = 0; i < nums.length; i++) {
+  //   total += nums[i];
+  // }
+  // return total / nums.length;
+}
+console.log(findAverage([1])); //1
+console.log(findAverage([1, 3, 5, 7])); //4
