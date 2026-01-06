9b445b76b30d92117e6d86e54661a2efd6753dc5
Your Name <ranamostafa2210@gmail.com>
2026-01-06 17:06:29 +0200

    Solve Crash Override problem

--- kata8.js
+++ kata8.js
@@ -1548,5 +1548,56 @@ function arrayMadness(a, b) {
     b.reduce((acc, cur) => cur ** 3 + acc, 0)
   );
 }
-console.log(arrayMadness([4, 5, 6], [1, 2, 3])); // true
-console.log(arrayMadness([5, 6, 7], [4, 5, 6])); // false
+// console.log(arrayMadness([4, 5, 6], [1, 2, 3])); // true
+// console.log(arrayMadness([5, 6, 7], [4, 5, 6])); // false
+
+/* (72) Crash Override
+Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override
+ are some notable examples from the film Hackers.
+
+Your task is to create a function that, given a proper first and last name, 
+will return the correct alias.
+
+Notes:
+Two objects that return a one word name in response to the first letter of the first name 
+and one for the first letter of the surname are already given. 
+See the examples below for further details.
+
+If the first character of either of the names given to the function is not a letter from A - Z, 
+you should return "Your name must start with a letter from A - Z."
+
+Sometimes people might forget to capitalize the first letter of their name 
+so your function should accommodate for these grammatical errors.
+
+Examples
+// These two objects are preloaded, you need to use them in your code
+var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+
+*/
+function aliasGen(first, last) {
+  var firstName = {
+    A: "Alpha",
+    B: "Beta",
+    C: "Cache",
+    D: "Data",
+    L: "Logic",
+    X: "Xerox",
+  };
+  var surname = {
+    A: "Analogue",
+    B: "Bomb",
+    C: "Catalyst",
+    D: "Data",
+    L: "Lazer",
+  };
+  return /[A-Z]/i.test(first[0]) && /[A-Z]/i.test(last[0])
+    ? firstName[first[0].toUpperCase()] + " " + surname[last[0].toUpperCase()]
+    : "Your name must start with a letter from A - Z.";
+}
+console.log(aliasGen("Larry", "Brentwood")); // Logic Bomb
+console.log(aliasGen("123abc", "Petrovic")); // Your name must start with a letter from A - Z.
+console.log(aliasGen("xaaq5", "lunjr")); // Xerox Lazer
