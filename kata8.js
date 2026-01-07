1c30f1cda7358aa255af5bd45aa391cd37279feb
Your Name <ranamostafa2210@gmail.com>
2026-01-07 20:31:51 +0200

    Solve Count words problem

--- kata8.js
+++ kata8.js
@@ -1733,4 +1733,35 @@ function correctPolishLetters(string) {
   //   .map((char) => polish[char] || char)
   //   .join("");
 }
-console.log(correctPolishLetters("Jędrzej Błądziński")); // "Jedrzej Bladzinski"
+// console.log(correctPolishLetters("Jędrzej Błądziński")); // "Jedrzej Bladzinski"
+
+/* (77) Count words
+Can you implement a function that will return number of words in a string?
+
+You have to ensure that spaces in string is a whitespace for real.
+
+Let's take a look on some examples:
+
+countWords("Hello"); // returns 1 as int
+countWords("Hello, World!") // returns 2
+countWords("No results for search term `s`") // returns 6
+countWords(" Hello") // returns 1
+// ... and so on
+What kind of tests we made for your code:
+
+Function have to count words and not spaces. You have to be sure that you doing it right
+Empty string has no words.
+String with spaces around should be trimmed.
+Non-whitespace (ex. breakspace, unicode chars) should be treated as a delimiter
+Doublecheck that words with chars like -, ', ` are counted right.
+*/
+function countWords(str) {
+  return str.split(/\s/).filter((item) => item !== "").length;
+  // \s = matches any whitespace char includes space or newline
+}
+console.log(countWords("Hello")); // 1
+console.log(countWords("Hello, World!")); // 2
+console.log(countWords("With! Symbol@ #Around! (Every) %Word$")); // 5
+console.log(countWords("Dear   Victoria, I love  to press   space button.")); // 8
+//  work with non-whitespace (ex. breakspace) chars
+console.log(countWords("Hello\u00A0World")); // 2
