af646c56361f61926b27b592845a724a1206fc0e
Your Name <ranamostafa2210@gmail.com>
2026-01-08 17:57:59 +0200

    Solve Safen User Input Part I - htmlspecialchars problem

--- kata8.js
+++ kata8.js
@@ -1893,5 +1893,63 @@ function apple(x) {
     ? "It's hotter than the sun!!"
     : "Help yourself to a honeycomb Yorkie for the glovebox.";
 }
-console.log(apple("50")); // "It's hotter than the sun!!"
-console.log(apple(4)); // "Help yourself to a honeycomb Yorkie for the glovebox."
+// console.log(apple("50")); // "It's hotter than the sun!!"
+// console.log(apple(4)); // "Help yourself to a honeycomb Yorkie for the glovebox."
+
+/* (83) Safen User Input Part I - htmlspecialchars
+Safen User Input Part I - htmlspecialchars
+You are a(n) novice/average/experienced/professional/world-famous Web Developer (choose one) who owns 
+a(n) simple/clean/slick/beautiful/complicated/professional/business website (choose one or more)
+ which contains form fields so visitors can send emails or leave a comment on your website with ease. 
+ However, with ease comes danger. 
+ Every now and then, a hacker visits your website and attempts to compromise it through the use of 
+ XSS (Cross Site Scripting). 
+ This is done by injecting script tags into the website through form fields which may contain malicious 
+ code (e.g. a redirection to a malicious website that steals personal information).
+
+Mission
+Your mission is to implement a function that converts the following potentially harmful characters:
+
+< --> &lt;
+> --> &gt;
+" --> &quot;
+& --> &amp;
+
+*/
+function htmlspecialchars(formData) {
+  return formData.replace(/[<>"&]/g, (char) => {
+    return {
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': "&quot;",
+      "&": "&amp;",
+    }[char];
+  });
+  //[OR]
+  // return formData.replace(
+  //   /[<>"&]/g,
+  //   (char) =>
+  //     ({
+  //       "<": "&lt;",
+  //       ">": "&gt;",
+  //       '"': "&quot;",
+  //       "&": "&amp;",
+  //     }[char])
+  // );
+  // [OR]
+  // return formData
+  //   .split("")
+  //   .map(
+  //     (char) =>
+  //       ({
+  //         "<": "&lt;",
+  //         ">": "&gt;",
+  //         '"': "&quot;",
+  //         "&": "&amp;",
+  //       }[char] || char)
+  //   )
+  //   .join("");
+}
+
+console.log(htmlspecialchars("<h2>Hello World</h2>")); // "&lt;h2&gt;Hello World&lt;/h2&gt;"
+console.log(htmlspecialchars("Hello, how would you & I fare?")); // "Hello, how would you &amp; I fare?"
