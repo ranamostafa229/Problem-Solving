01e4e2f7000ac529480eab1acab9b8fd6956196b
Your Name <ranamostafa2210@gmail.com>
2026-01-09 17:11:13 +0200

    Solve Reverse List Order problem

--- kata8.js
+++ kata8.js
@@ -1985,6 +1985,25 @@ Examples:
 function formatMoney(amount) {
   return `$${amount.toFixed(2)}`;
 }
-console.log(formatMoney(39.99)); // $39.99
-console.log(formatMoney(3)); // $3.00
-console.log(formatMoney(3.1)); // $3.10
+// console.log(formatMoney(39.99)); // $39.99
+// console.log(formatMoney(3)); // $3.00
+// console.log(formatMoney(3.1)); // $3.10
+
+/* (86) Reverse List Order
+In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+Examples (Input -> Output)
+* [1, 2, 3, 4]  -> [4, 3, 2, 1]
+* [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+*/
+function reverseList(list) {
+  return list.reverse();
+  // [OR]
+  // let output = [];
+  // for (let i = list.length - 1; i >= 0; i--) {
+  //   output.push(list[i]);
+  // }
+  // return output;
+}
+console.log(reverseList([1, 2, 3, 4])); // [4, 3, 2, 1]
