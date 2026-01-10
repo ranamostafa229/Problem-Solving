ec0b60c39d852042eb8c7e34acbf90982cdbbafd
Your Name <ranamostafa2210@gmail.com>
2026-01-10 17:08:38 +0200

    Solve Be Concise IV - Index of an element in an array problem

--- kata8.js
+++ kata8.js
@@ -2082,6 +2082,24 @@ function grader(score) {
     ? "C"
     : "D";
 }
-console.log(grader(0.7)); // "C"
-console.log(grader(0.9)); // "A"
-console.log(grader(1.1)); // "F"
+// console.log(grader(0.7)); // "C"
+// console.log(grader(0.9)); // "A"
+// console.log(grader(1.1)); // "F"
+
+/* (90) Be Concise IV - Index of an element in an array
+Task
+Provided is a function which accepts two parameters in the following order: array, element 
+and returns the index of the element if found and "Not found" otherwise. 
+Your task is to shorten the code as much as possible in order to meet the character count requirements
+of the Kata.
+
+You may assume that all array elements are unique.
+
+The character limit is 60 (CoffeeScript), 85 (JavaScript, Python), 161 (Java).
+
+*/
+function find(a, e) {
+  return a.indexOf(e) !== -1 ? a.indexOf(e) : "Not found";
+}
+console.log(find([1, 2, 3, 4], 3)); //2
+console.log(find([1, 2, 3, 4], 5)); //"Not found"
