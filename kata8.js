8547a31dff04be596aaba1bcc698899b7caaad98
Your Name <ranamostafa2210@gmail.com>
2026-01-10 17:34:45 +0200

    Solve Find the Integral problem

--- kata8.js
+++ kata8.js
@@ -2130,7 +2130,36 @@ is a single digit (0-9), false otherwise.
 String.prototype.digit = function () {
   return /^\d$/.test(this); // ^ => start of string, $ => end of string
 };
-console.log("3".digit()); //true
-console.log("".digit()); //false
-console.log("34".digit()); //false
-console.log("a".digit()); //false
+// console.log("3".digit()); //true
+// console.log("".digit()); //false
+// console.log("34".digit()); //false
+// console.log("a".digit()); //false
+
+/* (93) Find the Integral
+reate a function that finds the integral of the expression passed.
+
+In order to find the integral all you need to do is add one to the exponent (the second argument),
+ and divide the coefficient (the first argument) by that new number.
+
+For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent, 
+and divided the coefficient by that new number).
+
+Notes:
+
+The output should be a string.
+The coefficient and exponent is always a positive integer.
+Examples
+ 3, 2  -->  "1x^3"
+12, 5  -->  "2x^6"
+20, 1  -->  "10x^2"
+40, 3  -->  "10x^4"
+90, 2  -->  "30x^3"
+
+*/
+function integrate(coefficient, exponent) {
+  return coefficient / (exponent + 1) + "x^" + (exponent + 1);
+  // [OR]
+  // return `${coefficient / ++exponent}x^${exponent}`;
+}
+console.log(integrate(3, 2)); // "1x^3"
+console.log(integrate(12, 5)); //"2x^6"
