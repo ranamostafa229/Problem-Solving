cbca65ca6183dc6cca7d75898ae23bf2b4e5c5af
Your Name <ranamostafa2210@gmail.com>
2026-01-10 16:53:26 +0200

    Solve Grader problem

--- kata8.js
+++ kata8.js
@@ -2048,6 +2048,40 @@ function binToDec(bin) {
   // }
   // return decimal;
 }
-console.log(binToDec("0")); //0
-console.log(binToDec("1001001")); //73
-console.log(binToDec("1011"));
+// console.log(binToDec("0")); //0
+// console.log(binToDec("1001001")); //73
+// console.log(binToDec("1011"));
+
+/* (89) Grader
+Create a function that takes a number as an argument and returns a grade based on that number.
+
+Score	Grade
+Anything greater than 1 or less than 0.6	"F"
+0.9 or greater	"A"
+0.8 or greater	"B"
+0.7 or greater	"C"
+0.6 or greater	"D"
+Examples:
+
+grader(0)   should be "F"
+grader(1.1) should be "F"
+grader(0.9) should be "A"
+grader(0.8) should be "B"
+grader(0.7) should be "C"
+grader(0.6) should be "D"
+
+*/
+function grader(score) {
+  return score > 1 || score < 0.6
+    ? "F"
+    : score >= 0.9
+    ? "A"
+    : score >= 0.8
+    ? "B"
+    : score >= 0.7
+    ? "C"
+    : "D";
+}
+console.log(grader(0.7)); // "C"
+console.log(grader(0.9)); // "A"
+console.log(grader(1.1)); // "F"
