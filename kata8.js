e8590634e62d707fd5fca142490a136a55502a74
Your Name <ranamostafa2210@gmail.com>
2026-01-12 15:51:13 +0200

    Solve Is n divisible by x and y? probelm

--- kata8.js
+++ kata8.js
@@ -2386,4 +2386,20 @@ const replaceDots = function (str) {
   // [OR]
   return str.replaceAll(".", "-");
 };
-console.log(replaceDots("one.two.three")); // "one-two-three"
+// console.log(replaceDots("one.two.three")); // "one-two-three"
+
+/* (103) Is n divisible by x and y?
+Create a function that checks if a number n is divisible by two numbers x AND y. 
+All inputs are positive, non-zero numbers.
+
+Examples:
+1) n =   3, x = 1, y = 3 =>  true because   3 is divisible by 1 and 3
+2) n =  12, x = 2, y = 6 =>  true because  12 is divisible by 2 and 6
+3) n = 100, x = 5, y = 3 => false because 100 is not divisible by 3
+4) n =  12, x = 7, y = 5 => false because  12 is neither divisible by 7 nor 5
+*/
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0;
+}
+console.log(isDivisible(3, 3, 4)); // false
+console.log(isDivisible(12, 3, 4)); // true
