5a8b9ac587cb720fe01bf26ebc052487ae207f59
Your Name <ranamostafa2210@gmail.com>
2026-01-13 16:30:10 +0200

    Solve Be Concise II - I Need Squares problem

--- kata8.js
+++ kata8.js
@@ -2401,5 +2401,30 @@ Examples:
 function isDivisible(n, x, y) {
   return n % x === 0 && n % y === 0;
 }
-console.log(isDivisible(3, 3, 4)); // false
-console.log(isDivisible(12, 3, 4)); // true
+// console.log(isDivisible(3, 3, 4)); // false
+// console.log(isDivisible(12, 3, 4)); // true
+
+/* (104) Be Concise II - I Need Squares
+Be Concise II - I Need Squares
+You are given a program squaresOnly that accepts an array of natural numbers up to and including 100 
+(and including 0) of length >= 1, array, 
+and returns a new array containing only square numbers that have appeared in the input array.
+
+Refactor the solution to use as few characters as possible.
+ There is a maximum character limit of 127. Here are a few hints:
+
+There are a lot of handy built-in Array methods in Javascript that you may have never heard of even after
+completing a basic course in Javascript (e.g. those provided by Codecademy) 
+- well, at least I haven't heard of until quite recently. 
+You may also want to research any new built-in methods offered by ES6, the newest specification of 
+Javascript at the time of writing.
+Don't you think the array parameter is a bit wordy? ;)
+*/
+function squaresOnly(array) {
+  return array.filter((num) => Math.sqrt(num) % 1 === 0);
+  // [OR]
+  // return array.filter((num) => Number.isInteger(Math.sqrt(num)));
+  // isInteger() method determines whether the passed value is an integer without fraction.
+}
+console.log(squaresOnly([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // [0,1,4,9]
+console.log(squaresOnly([15, 16, 25, 36, 47, 88])); // [16,25,36]
