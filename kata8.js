48d2a565e76a8f7e6c64eb22c32acf3e91710135
Your Name <ranamostafa2210@gmail.com>
2026-01-14 16:32:54 +0200

    Solve Student's Final Grade problem

--- kata8.js
+++ kata8.js
@@ -2556,6 +2556,46 @@ The exponent will never be 1, and neither number will ever be 0
 function derive(coefficient, exponent) {
   return `${coefficient * exponent}x^${exponent - 1}`;
 }
-console.log(derive(7, 8)); // "56x^7"
-console.log(derive(5, 9)); // "45x^8"
-console.log(derive(9, 2)); // "18x^1"
+// console.log(derive(7, 8)); // "56x^7"
+// console.log(derive(5, 9)); // "45x^8"
+// console.log(derive(9, 2)); // "18x^1"
+
+/* (110) Student's Final Grade
+Create a function finalGrade, which calculates the final grade of a student depending on two parameters:
+a grade for the exam and a number of completed projects.
+
+This function should take two arguments: exam - grade for exam (from 0 to 100);
+ projects - number of completed projects (from 0 and above);
+
+This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+Examples(Inputs-->Output):
+
+100, 12 --> 100
+99, 0 --> 100
+10, 15 --> 100
+
+85, 5 --> 90
+
+55, 3 --> 75
+
+55, 0 --> 0
+20, 2 --> 0
+*Use Comparison and Logical Operators.
+*/
+function finalGrade(exam, projects) {
+  return exam > 90 || projects > 10
+    ? 100
+    : exam > 75 && projects >= 5
+    ? 90
+    : exam > 50 && projects >= 2
+    ? 75
+    : 0;
+}
+console.log(finalGrade(100, 12)); // 100
+console.log(finalGrade(85, 5)); // 90
+console.log(finalGrade(55, 3)); // 75
