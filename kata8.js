d4e8656807d38c38ffa22a74a051f0c85e4da8b3
Your Name <ranamostafa2210@gmail.com>
2026-01-14 16:25:47 +0200

    Solve Take the Derivative problem

--- kata8.js
+++ kata8.js
@@ -2529,5 +2529,33 @@ Example:
 function twoDecimalPlaces(n) {
   return +n.toFixed(2);
 }
-console.log(twoDecimalPlaces(4.659725356)); //4.66
-console.log(twoDecimalPlaces(173735326.3783732637948948)); //173735326.38
+// console.log(twoDecimalPlaces(4.659725356)); //4.66
+// console.log(twoDecimalPlaces(173735326.3783732637948948)); //173735326.38
+
+/* (109) Take the Derivative
+This function takes two numbers as parameters, the first number being the coefficient, 
+and the second number being the exponent.
+
+Your function should multiply the two numbers, and then subtract 1 from the exponent.
+ Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+For example:
+
+derive(7, 8)
+In this case, the function should multiply 7 and 8, and then subtract 1 from 8. 
+It should output "56x^7", the first number 56 being the product of the two numbers,
+ and the second number being the exponent minus 1.
+
+derive(7, 8) --> this should output "56x^7" 
+derive(5, 9) --> this should output "45x^8" 
+
+Notes:
+The output of this function should be a string
+The exponent will never be 1, and neither number will ever be 0
+*/
+function derive(coefficient, exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`;
+}
+console.log(derive(7, 8)); // "56x^7"
+console.log(derive(5, 9)); // "45x^8"
+console.log(derive(9, 2)); // "18x^1"
