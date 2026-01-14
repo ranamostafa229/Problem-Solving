9518ab8fee74af9fa34f7c34b5738ad818af394e
Your Name <ranamostafa2210@gmail.com>
2026-01-14 16:46:43 +0200

    Solve Incrementer problem

--- kata7.js
+++ kata7.js
@@ -48,5 +48,32 @@ function removeBMW(str) {
     ? new Error("This program only works for text.")
     : str.replace(/[bmw]/gi, "");
 }
-console.log(removeBMW("bmwvolvoBMW")); // "volvo"
-console.log(removeBMW(123)); // This program only works for text
+// console.log(removeBMW("bmwvolvoBMW")); // "volvo"
+// console.log(removeBMW(123)); // This program only works for text
+
+/* (3) Incrementer
+Given an input of an array of digits, return the array with each digit incremented by its position 
+in the array: the first digit will be incremented by 1, the second digit by 2, etc.
+ Make sure to start counting your positions from 1 ( and not 0 ).
+
+Your result can only contain single digit numbers, so if adding a digit with its position gives you
+a multiple-digit number, only the last digit of the number should be returned.
+
+Notes:
+return an empty array if your array is empty
+arrays will only contain numbers so don't worry about checking that
+Examples:
+[1, 2, 3]  -->  [2, 4, 6]   #  [1+1, 2+2, 3+3]
+
+[4, 6, 9, 1, 3]  -->  [5, 8, 2, 5, 8]  #  [4+1, 6+2, 9+3, 1+4, 3+5]  #  9+3 = 12  -->  2
+*/
+function incrementer(nums) {
+  return nums.map((num, index) => (num + index + 1) % 10);
+  // % 10 to get last digit when number is of base 10
+  // [OR]
+  // return nums
+  //   .map((num, index) => (num + index + 1).toString().slice(-1))
+  //   .map(Number);
+}
+console.log(incrementer([4, 6, 9, 1, 3])); // [5, 8, 2, 5, 8]
+console.log(incrementer([])); // []
