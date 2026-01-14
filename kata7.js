a2e0f3b8df07fc3e2fc44b9d55e665159d59ad20
Your Name <ranamostafa2210@gmail.com>
2026-01-14 17:28:19 +0200

    Solve Double Trouble problem

--- kata7.js
+++ kata7.js
@@ -88,5 +88,42 @@ All inputs will be valid.
 function digits(n) {
   return n.toString().length;
 }
-console.log(digits(128685)); // 6
-console.log(digits(0)); // 1
+// console.log(digits(128685)); // 6
+// console.log(digits(0)); // 1
+
+/* (5) Double Trouble
+Given an array of integers (x), and a target (t), you must find out if any two consecutive numbers
+ in the array sum to t. If so, remove the second number.
+
+Example:
+
+x = [1, 2, 3, 4, 5]
+t = 3
+
+1+2 = t, so remove 2. No other pairs = t, so rest of array remains:
+[1, 3, 4, 5]
+
+Work through the array from left to right.
+Return the resulting array.
+
+*/
+function trouble(x, t) {
+  let output = [];
+  for (let i = 0; i < x.length; i++) {
+    // If the last number in output + current number = t
+    if (x[i] + output[output.length - 1] === t) {
+      // Skip adding the current element (remove it)
+      continue;
+    }
+    output.push(x[i]);
+  }
+  return output;
+  // [OR]
+  // return x.reduce(
+  //   (acc, cur) => (cur + acc[acc.length - 1] !== t ? [...acc, cur] : acc),
+  //   []
+  // );
+}
+console.log(trouble([1, 3, 5, 6, 7, 4, 3], 7)); // [1, 3,5, 6, 7, 4]
+console.log(trouble([4, 1, 1, 1, 4], 2)); //  [4, 1, 4]
+console.log(trouble([2, 6, 2], 8)); // [2, 2]
