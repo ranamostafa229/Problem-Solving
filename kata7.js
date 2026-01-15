3a768e60da54fcd3bd080d8f8605622a8b59fba5
Your Name <ranamostafa2210@gmail.com>
2026-01-15 17:31:47 +0200

    Solve Player Contact Manager problem

--- kata7.js
+++ kata7.js
@@ -194,4 +194,41 @@ var sing = function () {
   }
   return lycrics;
 };
-console.log(sing());
+// console.log(sing());
+
+/* (7) Player Contact Manager
+You are the Dungeon Master for a public DnD game at your local comic shop and recently 
+you've had some trouble keeping your players' info neat and organized so you've decided
+ to write a bit of code to help keep them sorted!
+
+The goal of this code is to create an array of objects that stores a player's name and contact number 
+from a given string.
+
+The method should return an empty array if the argument passed is an empty string or nil/None/null.
+
+Examples
+player_manager("John Doe, 8167238327, Jane Doe, 8163723827") 
+returns [{player: "John Doe", contact: 8167238327}, {player: "Jane Doe", contact: 8163723827}]
+player_manager(nil) returns []
+player_manager("") returns []
+playerManager("John Doe, 8167238327, Jane Doe, 8163723827") 
+returns [{player: "John Doe", contact: 8167238327}, {player: "Jane Doe", contact: 8163723827}]
+playerManager(null) returns []
+playerManager("")   returns []
+
+*/
+function playerManager(players) {
+  if (players === null || players.trim() === "") return [];
+  return players
+    .split(",")
+    .reduce(
+      (acc, cur, index, arr) =>
+        index % 2 === 0
+          ? [...acc, { player: cur.trim(), contact: +arr[index + 1].trim() }]
+          : acc,
+      []
+    );
+}
+console.log(playerManager("John Doe, 8167238327, Jane Doe, 8163723827")); // [{player: "John Doe", contact: 8167238327}, {player: "Jane Doe", contact: 8163723827}]
+console.log(playerManager(null)); // []
+console.log(playerManager("")); // []
