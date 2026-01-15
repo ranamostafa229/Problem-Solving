d411dba3c002edb7b9820d42ba6942f638e3b64f
Your Name <ranamostafa2210@gmail.com>
2026-01-15 17:08:10 +0200

    Solve 99 Bottles of Beer problem

--- kata7.js
+++ kata7.js
@@ -124,6 +124,74 @@ function trouble(x, t) {
   //   []
   // );
 }
-console.log(trouble([1, 3, 5, 6, 7, 4, 3], 7)); // [1, 3,5, 6, 7, 4]
-console.log(trouble([4, 1, 1, 1, 4], 2)); //  [4, 1, 4]
-console.log(trouble([2, 6, 2], 8)); // [2, 2]
+// console.log(trouble([1, 3, 5, 6, 7, 4, 3], 7)); // [1, 3,5, 6, 7, 4]
+// console.log(trouble([4, 1, 1, 1, 4], 2)); //  [4, 1, 4]
+// console.log(trouble([2, 6, 2], 8)); // [2, 2]
+
+/* (6) 99 Bottles of Beer
+Instructions
+Complete the function that returns the lyrics for the song 99 Bottles of Beer as an array of strings:
+each line should be a separate element - see the example at the bottom.
+
+Note: in order to avoid hardcoded solutions, the size of your code is limited to 1000 characters
+
+Lyrics
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+
+98 bottles of beer on the wall, 98 bottles of beer.
+Take one down and pass it around, 97 bottles of beer on the wall.
+
+...and so on...
+
+3 bottles of beer on the wall, 3 bottles of beer.
+Take one down and pass it around, 2 bottles of beer on the wall.
+
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+
+1 bottle of beer on the wall, 1 bottle of beer.
+Take one down and pass it around, no more bottles of beer on the wall.
+
+No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+
+Example
+[ "99 bottles of beer on the wall, 99 bottles of beer.",
+  "Take one down and pass it around, 98 bottles of beer on the wall.",
+  "98 bottles of beer on the wall, 98 bottles of beer.",
+
+  ...and so on...
+
+  "3 bottles of beer on the wall, 3 bottles of beer.",
+  "Take one down and pass it around, 2 bottles of beer on the wall.",
+  "2 bottles of beer on the wall, 2 bottles of beer.",
+  "Take one down and pass it around, 1 bottle of beer on the wall.",
+  "1 bottle of beer on the wall, 1 bottle of beer.",
+  "Take one down and pass it around, no more bottles of beer on the wall.",
+  "No more bottles of beer on the wall, no more bottles of beer.",
+  "Go to the store and buy some more, 99 bottles of beer on the wall." ]
+*/
+var sing = function () {
+  let lycrics = [];
+  for (let i = 99; i >= 0; i--) {
+    i === 1
+      ? lycrics.push(
+          `${i} bottle of beer on the wall, ${i} bottle of beer.`,
+          `Take one down and pass it around, no more bottles of beer on the wall.`
+        )
+      : i > 1
+      ? lycrics.push(
+          `${i} bottles of beer on the wall, ${i} bottles of beer.`,
+          `Take one down and pass it around, ${i - 1} ${
+            i - 1 > 1 ? "bottles" : "bottle"
+          } of beer on the wall.`
+        )
+      : lycrics.push(
+          "No more bottles of beer on the wall, no more bottles of beer.",
+          `Go to the store and buy some more, 99 bottles of beer on the wall.`
+        );
+  }
+  return lycrics;
+};
+console.log(sing());
