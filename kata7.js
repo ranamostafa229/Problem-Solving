1ed1a2ad5220e3c41237e266b972b497f483bbf3
Your Name <ranamostafa2210@gmail.com>
2026-01-15 17:57:20 +0200

    Solve Character Counter problem

--- kata7.js
+++ kata7.js
@@ -280,6 +280,37 @@ The input string will only consist of lower case letters and/or spaces.
 function getCount(str) {
   return str.match(/[aeiou]/g)?.length || 0;
 }
-console.log(getCount("abracadabra")); // 5
-console.log(getCount("hello world")); // 3
-console.log(getCount("xyz")); // 0
+// console.log(getCount("abracadabra")); // 5
+// console.log(getCount("hello world")); // 3
+// console.log(getCount("xyz")); // 0
+
+/* (10) Character Counter
+You are going to be given a word. Your job will be to make sure that each character in that word has 
+the exact same number of occurrences. You will return true if it is valid, or false if it is not.
+
+For this kata, capitals are considered the same as lowercase letters. Therefore: "A" == "a"
+
+The input is a string (with no spaces) containing [a-z],[A-Z],[0-9] and common symbols. 
+The length will be 0 < length < 100.
+
+Examples
+"abcabc" is a valid word because "a" appears twice, "b" appears twice, and"c" appears twice.
+"abcabcd" is NOT a valid word because "a" appears twice, "b" appears twice, "c" appears twice, 
+ but "d" only appears once!
+"123abc!" is a valid word because all of the characters only appear once in the word.
+
+*/
+function validateWord(s) {
+  return (
+    new Set(
+      s
+        .toLowerCase()
+        .split("")
+        .map((char) => s.toLowerCase().split(char).length - 1)
+    ).size === 1
+  );
+}
+console.log(validateWord("abcabc")); // true
+console.log(validateWord("abcabcd")); // false
+console.log(validateWord("123abc!")); // true
+console.log(validateWord("abc:abc")); // false
