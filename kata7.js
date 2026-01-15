b9eb0773e7a04c21f5b04371caef48325ee7b88c
Your Name <ranamostafa2210@gmail.com>
2026-01-15 17:40:35 +0200

    Solve ISS - Keep It Simple Stupid problem

--- kata7.js
+++ kata7.js
@@ -229,6 +229,44 @@ function playerManager(players) {
       []
     );
 }
-console.log(playerManager("John Doe, 8167238327, Jane Doe, 8163723827")); // [{player: "John Doe", contact: 8167238327}, {player: "Jane Doe", contact: 8163723827}]
-console.log(playerManager(null)); // []
-console.log(playerManager("")); // []
+// console.log(playerManager("John Doe, 8167238327, Jane Doe, 8163723827")); // [{player: "John Doe", contact: 8167238327}, {player: "Jane Doe", contact: 8163723827}]
+// console.log(playerManager(null)); // []
+// console.log(playerManager("")); // []
+
+/* (8) ISS - Keep It Simple Stupid
+KISS stands for Keep It Simple Stupid. It is a design principle for keeping things simple rather than complex.
+
+You are the boss of Joe.
+
+Joe is submitting words to you to publish to a blog. He likes to complicate things.
+
+Define a function that determines if Joe's work is simple or complex.
+
+Input will be non emtpy strings with no punctuation.
+
+It is simple if: the length of each word does not exceed the amount of words in the string
+(See example test cases)
+
+Otherwise it is complex.
+
+If complex:
+
+return "Keep It Simple Stupid"
+or if it was kept simple:
+
+return "Good work Joe!"
+Note: Random test are random and nonsensical. Here is a silly example of a random test:
+
+"jump always mostly is touchy dancing choice is pineapples mostly"
+
+*/
+function isKiss(words) {
+  return words
+    .split(" ")
+    .every((word) => word.length <= words.split(" ").length)
+    ? "Good work Joe!"
+    : "Keep It Simple Stupid";
+}
+
+console.log(isKiss("Joe had a bad day")); // "Good work Joe!"
+console.log(isKiss("Sometimes joe cries for hours")); // "Keep It Simple Stupid"
