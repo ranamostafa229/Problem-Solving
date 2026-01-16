11057694bffbbdc4c318935fe04f8a7da1f3a1c9
Your Name <ranamostafa2210@gmail.com>
2026-01-16 15:51:47 +0200

    Solve Band name generator problem

--- kata7.js
+++ kata7.js
@@ -310,7 +310,31 @@ function validateWord(s) {
     ).size === 1
   );
 }
-console.log(validateWord("abcabc")); // true
-console.log(validateWord("abcabcd")); // false
-console.log(validateWord("123abc!")); // true
-console.log(validateWord("abc:abc")); // false
+// console.log(validateWord("abcabc")); // true
+// console.log(validateWord("abcabcd")); // false
+// console.log(validateWord("123abc!")); // true
+// console.log(validateWord("abc:abc")); // false
+
+/* (11) Band name generator
+My friend wants a new band name for her band. She like bands that use the formula: 
+"The" + a noun with the first letter capitalized, for example:
+
+"dolphin" -> "The Dolphin"
+
+However, when a noun STARTS and ENDS with the same letter, she likes to repeat 
+the noun twice and connect them together with the first and last letter, 
+combined into one word (WITHOUT "The" in front), like this:
+
+"alaska" -> "Alaskalaska"
+
+Complete the function that takes a noun as a string, and returns her preferred band name written 
+as a string.
+
+*/
+function bandNameGenerator(str) {
+  return str[str.length - 1].toLowerCase() === str[0].toLowerCase()
+    ? str[0].toUpperCase() + str.slice(1) + str.slice(1)
+    : `The ${str[0].toUpperCase() + str.slice(1)}`;
+}
+// console.log(bandNameGenerator("knife")); // "The Knife"
+// console.log(bandNameGenerator("tart")); // Tartart
