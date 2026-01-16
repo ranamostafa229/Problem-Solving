bb64f49ab3da29642a1ecd787b386eafa65f766d
Your Name <ranamostafa2210@gmail.com>
2026-01-16 16:06:56 +0200

    Solve Odds-Index problem

--- kata7.js
+++ kata7.js
@@ -338,3 +338,53 @@ function bandNameGenerator(str) {
 }
 // console.log(bandNameGenerator("knife")); // "The Knife"
 // console.log(bandNameGenerator("tart")); // Tartart
+
+/* (12) Odds-Index
+You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
+
+Determine if any of the numbers in the array is the index of the "odd" word. 
+If so, return true, otherwise false.
+
+*/
+function oddBall(arr) {
+  return arr.find((num) => num === arr.indexOf("odd")) ? true : false;
+  // [OR]
+  // return arr.includes(arr.indexOf("odd"));
+  // [OR]
+  // return arr.some((num) => arr[num] === "odd");
+}
+console.log(
+  oddBall([
+    "even",
+    4,
+    "even",
+    7,
+    "even",
+    55,
+    "even",
+    6,
+    "even",
+    10,
+    "odd",
+    3,
+    "even",
+  ])
+); // true
+console.log(
+  oddBall([
+    "even",
+    4,
+    "even",
+    7,
+    "even",
+    55,
+    "even",
+    6,
+    "even",
+    9,
+    "odd",
+    3,
+    "even",
+  ])
+); // false
+console.log(oddBall(["even", 10, "odd", 2, "even"])); // true
