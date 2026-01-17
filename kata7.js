51c24cfa1cbe8ff4548f9cf4e5108491baad1b46
Your Name <ranamostafa2210@gmail.com>
2026-01-17 16:52:09 +0200

    Solve Change two -dimensional array problem

--- kata7.js
+++ kata7.js
@@ -507,6 +507,67 @@ function sumDigits(number) {
     0
   );
 }
-console.log(sumDigits(10)); // 1
-console.log(sumDigits(99)); // 18
-console.log(sumDigits(-32)); // 5
+// console.log(sumDigits(10)); // 1
+// console.log(sumDigits(99)); // 18
+// console.log(sumDigits(-32)); // 5
+
+/* (17) Change two-dimensional array
+Function receive a two-dimensional square array of random integers. On the main diagonal, 
+all the negative integers must be changed to 0, while the others must be changed to 1 
+(Note: 0 is considered non-negative, here).
+
+(You can mutate the input if you want, but it is a better practice to not mutate the input)
+
+Example:
+
+Input array
+
+[
+  [-1,  4, -5, -9,  3 ],
+  [ 6, -4, -7,  4, -5 ],
+  [ 3,  5,  0, -9, -1 ],
+  [ 1,  5, -7, -8, -9 ],
+  [-3,  2,  1, -5,  6 ]
+]
+Output array
+
+[
+  [ 0,  4, -5, -9,  3 ],
+  [ 6,  0, -7,  4, -5 ],
+  [ 3,  5,  1, -9, -1 ],
+  [ 1,  5, -7,  0, -9 ],
+  [-3,  2,  1, -5,  1 ]
+]
+
+*/
+function matrix(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (i === j) {
+        array[i][j] = array[i][j] >= 0 ? 1 : 0;
+      }
+    }
+  }
+  return array;
+  // [OR]
+  // for (let i = 0; i < array.length; i++) {
+  //   array[i][i] = array[i][i] >= 0 ? 1 : 0;
+  // } // as we need the index where row index = column index
+  // return array;
+}
+console.log(
+  matrix([
+    [-1, 4, -5, -9, 3],
+    [6, -4, -7, 4, -5],
+    [3, 5, 4, -9, -1],
+    [1, 5, -7, -8, -9],
+    [-3, 2, 1, -5, 6],
+  ])
+);
+// [
+//   [ 0,  4, -5, -9,  3 ],
+//   [ 6,  0, -7,  4, -5 ],
+//   [ 3,  5,  1, -9, -1 ],
+//   [ 1,  5, -7,  0, -9 ],
+//   [-3,  2,  1, -5,  1 ]
+// ]
