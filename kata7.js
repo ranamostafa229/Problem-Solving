331fbcce5aa8c59fb1b3757f6d6e32356f1364c6
Your Name <ranamostafa2210@gmail.com>
2026-01-18 17:48:44 +0200

    Solve Capitals first! problem

--- kata7.js
+++ kata7.js
@@ -713,6 +713,38 @@ e.g. 'a' and 'A' are considered different characters.
 function hasUniqueChars(str) {
   return new Set(str).size === str.length;
 }
-console.log(hasUniqueChars("  nAa")); // false
-console.log(hasUniqueChars("abcdef")); // true
-console.log(hasUniqueChars("aA")); // true
+// console.log(hasUniqueChars("  nAa")); // false
+// console.log(hasUniqueChars("abcdef")); // true
+// console.log(hasUniqueChars("aA")); // true
+
+/* (24) Capitals First
+Create a function that takes an input String and returns a String, where all the uppercase words 
+of the input String are in front and all the lowercase words at the end. 
+The order of the uppercase and lowercase words should be the order in which they occur.
+
+If a word starts with a number or special character, skip the word and leave it out of the result.
+
+Input String will not be empty.
+
+For an input String: "hey You, Sort me Already!" the function should return: "You, Sort Already! hey me"
+
+*/
+function capitalsFirst(str) {
+  const words = str.split(" ");
+  const upper = words.filter((word) => /^[A-Z]/.test(word));
+  const lower = words.filter((word) => /^[a-z]/.test(word));
+  return [...upper, ...lower].join(" ");
+  // [OR]
+  // return str
+  //   .split(" ")
+  //   .sort((a, b) => {
+  //     if (/^[A-Z]/.test(a) && /^[a-z]/.test(b)) {
+  //       return -1;
+  //     } else if (/^[a-z]/.test(a) && /^[A-Z]/.test(b)) {
+  //       return 1;
+  //     }
+  //     return 0;
+  //   })
+  //   .join(" ");
+}
+console.log(capitalsFirst("hey You, Sort me Already!")); // "You, Sort Already! hey me"
