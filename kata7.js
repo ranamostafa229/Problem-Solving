422450e03cb4fc3ca03109bdf817b0e0778b9eef
Your Name <ranamostafa2210@gmail.com>
2026-01-19 16:58:12 +0200

    Solve Sum Array with different bases problem

--- kata7.js
+++ kata7.js
@@ -812,6 +812,43 @@ solution(null); // should return []
 function solution(nums) {
   return nums?.sort((a, b) => a - b) || [];
 }
-console.log(solution([1, 2, 3, 10, 5])); // [1,2,3,5,10]
-console.log(solution(null)); // []
-console.log(solution([])); // []
+// console.log(solution([1, 2, 3, 10, 5])); // [1,2,3,5,10]
+// console.log(solution(null)); // []
+// console.log(solution([])); // []
+
+/* (28) Sum Array with different bases
+You get an array of different numbers to sum up. But there is one problem, those numbers all have 
+different bases. For example:
+
+You get an array of numbers with their base as an input:
+
+[["101",16],["7640",8],["1",9]]
+The output should be the sum as an integer value with a base of 10, so according to the example 
+this would be:
+
+4258
+
+A few more examples:
+[["101",2], ["10",8]] --> 13
+[["ABC",16], ["11",2]] --> 2751
+Bases can be between 2 and 36 (2<=base<=36)
+
+*/
+function sumItUp(numbersWithBases) {
+  return numbersWithBases.reduce(
+    (acc, cur) => acc + parseInt(cur[0], cur[1]),
+    0,
+  );
+}
+console.log(
+  sumItUp([
+    ["101", 2],
+    ["10", 8],
+  ]),
+); // 13
+console.log(
+  sumItUp([
+    ["ABC", 16],
+    ["11", 2],
+  ]),
+); // 2751
