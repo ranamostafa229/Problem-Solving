826fee369440b7152cd074aba9a54574672bc14f
Your Name <ranamostafa2210@gmail.com>
2026-01-19 16:48:42 +0200

    Solve Sort Numbers problem

--- kata7.js
+++ kata7.js
@@ -797,5 +797,21 @@ function findEmployeesRole(name) {
     )?.role || "Does not work here!"
   );
 }
-console.log(findEmployeesRole("Dipper Pines")); // Does not work here!
-console.log(findEmployeesRole("Morty Smith")); // Truck Driver"
+// console.log(findEmployeesRole("Dipper Pines")); // Does not work here!
+// console.log(findEmployeesRole("Morty Smith")); // Truck Driver"
+
+/* (27) Sort Numbers
+Finish the solution so that it sorts the passed in array of numbers. 
+If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+*/
+function solution(nums) {
+  return nums?.sort((a, b) => a - b) || [];
+}
+console.log(solution([1, 2, 3, 10, 5])); // [1,2,3,5,10]
+console.log(solution(null)); // []
+console.log(solution([])); // []
