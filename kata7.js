f6f7184bed6327e834400229ad511c3392b6a63a
Your Name <ranamostafa2210@gmail.com>
2026-01-20 17:10:13 +0200

    Solve Average Array problem

--- kata7.js
+++ kata7.js
@@ -924,7 +924,71 @@ const isAnagram = function (test, original) {
   const testSorted = (str) => str.toLowerCase().split("").sort().join("");
   return testSorted(test) === testSorted(original);
 };
-console.log(isAnagram("foefet", "toffee")); // true
-console.log(isAnagram("Buckethead", "DeathCubeK")); // true
-console.log(isAnagram("ound", "round")); // false
-console.log(isAnagram("dumble", "bumble")); // false
+// console.log(isAnagram("foefet", "toffee")); // true
+// console.log(isAnagram("Buckethead", "DeathCubeK")); // true
+// console.log(isAnagram("ound", "round")); // false
+// console.log(isAnagram("dumble", "bumble")); // false
+
+/* (32) Average Array
+ASC Week 1 Challenge 5 (Medium #2)
+Create a function that takes a 2D array as an input, and outputs another array that 
+contains the average values for the numbers in the nested arrays at the corresponding indexes.
+
+Note: the function should also work with negative numbers and floats.
+
+Examples
+[ [1, 2, 3, 4], [5, 6, 7, 8] ]  ==>  [3, 4, 5, 6]
+
+1st array: [1, 2, 3, 4]
+2nd array: [5, 6, 7, 8]
+            |  |  |  |
+            v  v  v  v
+average:   [3, 4, 5, 6]
+And another one:
+
+[ [2, 3, 9, 10, 7], [12, 6, 89, 45, 3], [9, 12, 56, 10, 34], [67, 23, 1, 88, 34] ]  
+ ==>  [22.5, 11, 38.75, 38.25, 19.5]
+
+1st array: [  2,   3,    9,   10,    7]
+2nd array: [ 12,   6,   89,   45,    3]
+3rd array: [  9,  12,   56,   10,   34]
+4th array: [ 67,  23,    1,   88,   34]
+              |    |     |     |     |
+              v    v     v     v     v
+average:   [22.5, 11, 38.75, 38.25, 19.5]
+
+*/
+function avgArray(arr) {
+  return Array.from(
+    { length: arr[0].length },
+    (_, i) => arr.reduce((sum, row) => sum + row[i], 0) / arr.length,
+  );
+  // [OR]
+  // return arr[0].map(
+  //   (_, i) => arr.reduce((sum, row) => sum + row[i], 0) / arr.length,
+  // );
+  // [OR]
+  // let result = [];
+  // for (let i = 0; i < arr[0].length; i++) {
+  //   let sum = 0;
+  //   for (let j = 0; j < arr.length; j++) {
+  //     sum += arr[j][i];
+  //   }
+  //   result.push(sum / arr.length);
+  // }
+  // return result;
+}
+console.log(
+  avgArray([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+  ]),
+); // [3, 4, 5, 6]
+console.log(
+  avgArray([
+    [2, 3, 9, 10, 7],
+    [12, 6, 89, 45, 3],
+    [9, 12, 56, 10, 34],
+    [67, 23, 1, 88, 34],
+  ]),
+); //[22.5, 11, 38.75, 38.25, 19.5]
