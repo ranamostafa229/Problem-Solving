b6ed1b3f772aeb87ace4f03ba84869b6140b8ada
Your Name <ranamostafa2210@gmail.com>
2026-01-22 18:03:49 +0200

    Solve Argue the toss problem

--- kata7.js
+++ kata7.js
@@ -1139,7 +1139,55 @@ function initializeNames(name) {
     )
     .join(" ");
 }
-console.log(initializeNames("Alice Betty Catherine Davis")); // "Alice B. C. Davis"
-console.log(initializeNames("Dimitri")); // "Dimitri"
-console.log(initializeNames("Lois Mary Lane")); // "Lois M. Lane"
-console.log(initializeNames("Jack Ryan")); // "Jack Ryan"
+// console.log(initializeNames("Alice Betty Catherine Davis")); // "Alice B. C. Davis"
+// console.log(initializeNames("Dimitri")); // "Dimitri"
+// console.log(initializeNames("Lois Mary Lane")); // "Lois M. Lane"
+// console.log(initializeNames("Jack Ryan")); // "Jack Ryan"
+
+/* (40) Argue the toss
+Your task is to write a function that doesn't have a fixed number of parameters. 
+The function should count the arguments passed to it and return a string that specifies 
+how many arguments were passed in, and states what they were.
+
+If the function call is
+
+anArgument("pigs", "giraffes", "unicorns");
+The returned string should say
+
+'You gave me 3 arguments and they are "pigs", "giraffes" and "unicorns".'
+If there is only one argument passed in, the string should say
+
+'You gave me 1 argument and it is "pigs".'
+If there are two arguments, the string should say
+
+'You gave me 2 arguments and they are "pigs" and "giraffes".'
+If there are no arguments, the string should say
+
+'You didn't give me any arguments.'
+Punctuation and grammar notes
+There are commas after each argument except the last and second-last, which are separated with 'and'
+Arguments are presented in quotation marks
+The singular: '1 argument' as opposed to multiples: '3 arguments'
+Verb agreement: 'and it is' as opposed to 'and they are'
+
+*/
+function anArgument(...arg) {
+  if (arg.length === 0) return "You didn't give me any arguments.";
+  return arg.length >= 2
+    ? `You gave me ${arg.length} arguments and they are ${
+        arg.length > 2
+          ? arg
+              .slice(0, -1)
+              .map((a) => `"${a}"`)
+              .join(", ") +
+            " and " +
+            `"${arg[arg.length - 1]}"`
+          : `"${arg[0]}" and "${arg[1]}"`
+      }.`
+    : `You gave me 1 argument and it is "${arg[0]}".`;
+}
+console.log(anArgument("chairs", "table", "lamp", "sideboard")); // "You gave me 4 arguments and they are "chairs", "table", "lamp" and "sideboard"."
+console.log(anArgument("pigs", "giraffes", "unicorns")); // "You gave me 3 arguments and they are "pigs", "giraffes" and "unicorns"."
+console.log(anArgument("pigs", "giraffes")); // "You gave me 2 arguments and they are "pigs" and "giraffes"."
+console.log(anArgument("pigs")); // "You gave me 1 argument and it is "pigs"."
+console.log(anArgument()); // "You didn't give me any arguments."
