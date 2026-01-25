63a769501f6c53ae13e2a797466ac33317e062de
Your Name <ranamostafa2210@gmail.com>
2026-01-25 15:42:04 +0200

    Solve String matchup problem

--- kata7.js
+++ kata7.js
@@ -1458,5 +1458,26 @@ function recycleMe(recycle) {
     [0, 0, 0],
   );
 }
-console.log(recycleMe([5, -9, 0, 6, -84, -95, 15])); // [3, 3, 1]
-console.log(recycleMe([45, -26, -4, -66, -84, -38, 14])); // [2, 5, 0]
+// console.log(recycleMe([5, -9, 0, 6, -84, -95, 15])); // [3, 3, 1]
+// console.log(recycleMe([45, -26, -4, -66, -84, -38, 14])); // [2, 5, 0]
+
+/* (51) String matchup
+Given two arrays of strings, return the number of times each string of the second array appears
+in the first array.
+
+Example
+array1 = ['abc', 'abc', 'xyz', 'cde', 'uvw']
+array2 = ['abc', 'cde', 'uap']
+How many times do the elements in array2 appear in array1?
+
+'abc' appears twice in the first array (2)
+'cde' appears only once (1)
+'uap' does not appear in the first array (0)
+Therefore, solve(array1, array2) = [2, 1, 0]
+
+*/
+function solve(a, b) {
+  return b.map((e) => a.filter((f) => f === e).length);
+}
+console.log(solve(["abc", "abc", "xyz", "abcd", "cde"], ["abc", "cde", "uap"])); // [2, 1, 0]
+console.log(solve(["abc", "xyz", "abc", "xyz", "cde"], ["abc", "cde", "xyz"])); // [2, 1, 2]
