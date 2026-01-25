7c3f8889e094ad2b0106dd941ccc165838820dff
Your Name <ranamostafa2210@gmail.com>
2026-01-25 17:07:51 +0200

    Solve String doubles problem

--- kata7.js
+++ kata7.js
@@ -1577,6 +1577,41 @@ reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
 function reject(array, predicate) {
   return array.filter((x) => !predicate(x));
 }
-console.log(reject(["a", "b", 3, "d"], (x) => typeof x === "string")); // [3]
-console.log(reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)); // [1, 3, 5]
-console.log(reject(["a", "b", 3, "d"], (x) => typeof x === "number")); // ['a', 'b', 'd']
+// console.log(reject(["a", "b", 3, "d"], (x) => typeof x === "string")); // [3]
+// console.log(reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)); // [1, 3, 5]
+// console.log(reject(["a", "b", 3, "d"], (x) => typeof x === "number")); // ['a', 'b', 'd']
+
+/* (55) String doubles
+In this Kata, you will write a function doubles that will remove double string characters 
+that are adjacent to each other.
+
+For example:
+
+doubles('abbcccdddda') = 'aca', because, from left to right:
+
+a) There is only one 'a' on the left hand side, so it stays.
+b) The 2 b's disappear because we are removing double characters that are adjacent. 
+c) Of the 3 c's, we remove two. We are only removing doubles. 
+d) The 4 d's all disappear, because we first remove the first double, and again we remove the second double.
+e) There is only one 'a' at the end, so it stays.
+Two more examples: doubles('abbbzz') = 'ab' and doubles('abba') = "". In the second example,
+when we remove the b's in 'abba', the double a that results is then removed.
+
+The strings will contain lowercase letters only. More examples in the test cases.
+
+*/
+function doubles(s) {
+  const stack = [];
+  for (const char of s) {
+    if (stack[stack.length - 1] === char) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.join("");
+}
+console.log(doubles("abbbzz")); // 'ab'
+console.log(doubles("zzzzykkkd")); // 'ykd'
+console.log(doubles("xxbnnnnnyaaaaam")); // 'bnyam'
+console.log(doubles("rrrmooomqqqqj")); // 'rmomj
