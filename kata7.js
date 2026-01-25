4b47ca03d24dad4639f9576da1b0a305708061e6
Your Name <ranamostafa2210@gmail.com>
2026-01-25 15:50:51 +0200

    Solve Simple string reversal II problem

--- kata7.js
+++ kata7.js
@@ -1479,5 +1479,29 @@ Therefore, solve(array1, array2) = [2, 1, 0]
 function solve(a, b) {
   return b.map((e) => a.filter((f) => f === e).length);
 }
-console.log(solve(["abc", "abc", "xyz", "abcd", "cde"], ["abc", "cde", "uap"])); // [2, 1, 0]
-console.log(solve(["abc", "xyz", "abc", "xyz", "cde"], ["abc", "cde", "xyz"])); // [2, 1, 2]
+// console.log(solve(["abc", "abc", "xyz", "abcd", "cde"], ["abc", "cde", "uap"])); // [2, 1, 0]
+// console.log(solve(["abc", "xyz", "abc", "xyz", "cde"], ["abc", "cde", "xyz"])); // [2, 1, 2]
+
+/* (52) Simple string reversal II
+In this Kata, you will be given a string and two indexes (a and b). 
+Your task is to reverse the portion of that string between those two indices inclusive.
+
+str = "codewars", a = 1, b = 5 --> "cawedors"
+str = "cODEWArs", a = 1, b = 5 --> "cAWEDOrs"
+Input will be lowercase and uppercase letters only.
+
+The first index a will always be smaller than the string length; 
+the second index b can be greater than the string length. More examples in the test cases. Good luck!
+
+*/
+function solve(st, a, b) {
+  return (
+    st.slice(0, a) +
+    [...st.slice(a, b + 1)].reverse().join("") +
+    st.slice(b + 1)
+  );
+}
+console.log(solve("codewars", 1, 5)); // "cawedors"
+console.log(solve("cODEWArs", 1, 5)); // "cAWEDOrs"
+console.log(solve("FunctionalProgramming", 2, 15)); // "FuargorPlanoitcnmming"
+console.log(solve("codingIsFun", 2, 100)); // "conuFsIgnid"
