ec3028d1f4c1e99c309595064caaf739962cbbe0
Your Name <ranamostafa2210@gmail.com>
2026-01-25 16:30:33 +0200

    Solve Alphabet war problem

--- kata7.js
+++ kata7.js
@@ -1501,7 +1501,69 @@ function solve(st, a, b) {
     st.slice(b + 1)
   );
 }
-console.log(solve("codewars", 1, 5)); // "cawedors"
-console.log(solve("cODEWArs", 1, 5)); // "cAWEDOrs"
-console.log(solve("FunctionalProgramming", 2, 15)); // "FuargorPlanoitcnmming"
-console.log(solve("codingIsFun", 2, 100)); // "conuFsIgnid"
+// console.log(solve("codewars", 1, 5)); // "cawedors"
+// console.log(solve("cODEWArs", 1, 5)); // "cAWEDOrs"
+// console.log(solve("FunctionalProgramming", 2, 15)); // "FuargorPlanoitcnmming"
+// console.log(solve("codingIsFun", 2, 100)); // "conuFsIgnid"
+
+/* (53) Alphabet war
+Introduction
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side letters and right side letters
+ was too high and the war began.
+
+Task
+Write a function that accepts fight string consists of only small letters and return who wins the fight.
+When the left side wins return Left side wins!, when the right side wins return Right side wins!, 
+in other case return Let's fight again!.
+
+The left side letters and their power:
+
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+The right side letters and their power:
+
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+The other letters don't have power and are only victims. Sum up each side's letters' power values 
+to determine which side wins.
+
+Example
+alphabetWar("z");        //=> Right side wins!
+alphabetWar("zdqmwpbs"); //=> Let's fight again!
+alphabetWar("zzzzs");    //=> Right side wins!
+alphabetWar("wwwwwwz");  //=> Left side wins!
+
+*/
+function alphabetWar(fight) {
+  const left = {
+    w: 4,
+    p: 3,
+    b: 2,
+    s: 1,
+  };
+  const right = {
+    m: 4,
+    q: 3,
+    d: 2,
+    z: 1,
+  };
+
+  const score = [...fight].reduce(
+    (acc, cur) => acc + (left[cur] || 0) - (right[cur] || 0),
+    0,
+  );
+  return score > 0
+    ? "Left side wins!"
+    : score < 0
+      ? "Right side wins!"
+      : "Let's fight again!";
+}
+console.log(alphabetWar("z")); // => Right side wins!
+console.log(alphabetWar("zdqmwpbs")); // => Let's fight again!
+console.log(alphabetWar("zzzzs")); // => Right side wins!
+console.log(alphabetWar("wwwwwwz")); // => Left side wins!
