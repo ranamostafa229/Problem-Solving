f35395e54de71dd2841f4f02cdfe14269556a0c4
Your Name <ranamostafa2210@gmail.com>
2026-01-26 17:11:47 +0200

    Solve Help the Fruit Guy problem

--- kata7.js
+++ kata7.js
@@ -1611,7 +1611,30 @@ function doubles(s) {
   }
   return stack.join("");
 }
-console.log(doubles("abbbzz")); // 'ab'
-console.log(doubles("zzzzykkkd")); // 'ykd'
-console.log(doubles("xxbnnnnnyaaaaam")); // 'bnyam'
-console.log(doubles("rrrmooomqqqqj")); // 'rmomj
+// console.log(doubles("abbbzz")); // 'ab'
+// console.log(doubles("zzzzykkkd")); // 'ykd'
+// console.log(doubles("xxbnnnnnyaaaaam")); // 'bnyam'
+// console.log(doubles("rrrmooomqqqqj")); // 'rmomj
+
+/* (56) Help the Fruit Guy
+Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten.
+He wants to replace all the rotten pieces of fruit with fresh ones. 
+For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"].
+Your task is to implement a method that accepts an array of strings containing fruits should returns an array
+of strings where all the rotten fruits are replaced by good ones.
+
+Notes
+If the array is null/nil/None or empty you should return empty array ([]).
+The rotten fruit name will be in this camelcase (rottenFruit).
+The returned array should be in lowercase.
+
+*/
+function removeRotten(bagOfFruits) {
+  return (
+    bagOfFruits?.map((fruit) => fruit.replace("rotten", "").toLowerCase()) || []
+  );
+}
+console.log(removeRotten(["apple", "banana", "kiwi", "melone", "orange"])); // ["apple","banana","kiwi","melon","orange"]
+console.log(removeRotten(["apple", "rottenBanana", "apple"])); // ["apple","banana","apple"]
+console.log(removeRotten([])); // []
+console.log(removeRotten(null)); // []
