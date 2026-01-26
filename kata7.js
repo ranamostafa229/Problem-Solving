08245d8bb1736b3aadc90ac69d0b8c7097ceec11
Your Name <ranamostafa2210@gmail.com>
2026-01-26 17:45:43 +0200

    Solve Likes Vs Dislikes problem

--- kata7.js
+++ kata7.js
@@ -1683,21 +1683,53 @@ function itinerary(travel) {
     .join("-");
 }
 
-console.log(
-  itinerary([
-    { in: "TRN", out: "FCO" },
-    { in: "CIA", out: "JFK" },
-  ]),
-); //"TRN-FCO-CIA-JFK"
-console.log(
-  itinerary([
-    { in: "TRN", out: "FCO" },
-    { in: "FCO", out: "JFK" },
-  ]),
-); // "TRN-FCO-JFK"
-console.log(
-  itinerary([
-    { in: "TRN", out: "FCO" },
-    { in: "CIA", out: "TRN" },
-  ]),
-); // "TRN-FCO-CIA-TRN"
+// console.log(
+//   itinerary([
+//     { in: "TRN", out: "FCO" },
+//     { in: "CIA", out: "JFK" },
+//   ]),
+// ); //"TRN-FCO-CIA-JFK"
+// console.log(
+//   itinerary([
+//     { in: "TRN", out: "FCO" },
+//     { in: "FCO", out: "JFK" },
+//   ]),
+// ); // "TRN-FCO-JFK"
+// console.log(
+//   itinerary([
+//     { in: "TRN", out: "FCO" },
+//     { in: "CIA", out: "TRN" },
+//   ]),
+// ); // "TRN-FCO-CIA-TRN"
+
+/* (58) Likes Vs Dislikes
+Story
+YouTube had a like and a dislike button, which allowed users to express their opinions about particular content.
+It was set up in such a way that you cannot like and dislike a video at the same time.
+There are two other interesting rules to be noted about the interface: Pressing a button,
+which is already active, will undo your press. If you press the like button after pressing the dislike button,
+the like button overwrites the previous "Dislike" state. The same is true for the other way round.
+
+Task
+Create a function that takes in a list of button inputs and returns the final state.
+
+Examples
+likeOrDislike([Dislike]) => Dislike
+likeOrDislike([Like,Like]) => Nothing
+likeOrDislike([Dislike,Like]) => Like
+likeOrDislike([Like,Dislike,Dislike]) => Nothing
+Notes
+If no button is currently active, return Nothing.
+If the list is empty, return Nothing.
+
+*/
+function likeOrDislike(buttons) {
+  return buttons.reduce(
+    (acc, cur) => (cur === acc ? "Nothing" : cur),
+    "Nothing",
+  );
+}
+console.log(likeOrDislike(["Dislike"])); // Dislike
+console.log(likeOrDislike(["Like", "Like"])); // Nothing
+console.log(likeOrDislike(["Dislike", "Like"])); // Like
+console.log(likeOrDislike(["Like", "Dislike", "Dislike"])); // Nothing
