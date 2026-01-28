ddc025c7cdd583c20537ee0b534decb9ceb8d082
Your Name <ranamostafa2210@gmail.com>
2026-01-28 17:35:56 +0200

    Solve Is it a vowel on this position? problem

--- kata7.js
+++ kata7.js
@@ -1831,8 +1831,30 @@ function sentencify(words) {
   // [OR]
   // return words.join(" ").replace(/^[a-z]/, (w) => w.toUpperCase()) + ".";
 }
-console.log(sentencify(["i", "am", "an", "AI"])); // "I am an AI."
-console.log(sentencify(["yes"])); // "Yes."
-console.log(
-  sentencify(["i'm", "afraid", "I", "can't", "let", "you", "do", "that"]),
-); // "I'm afraid I can't let you do that."
+// console.log(sentencify(["i", "am", "an", "AI"])); // "I am an AI."
+// console.log(sentencify(["yes"])); // "Yes."
+// console.log(
+//   sentencify(["i'm", "afraid", "I", "can't", "let", "you", "do", "that"]),
+// ); // "I'm afraid I can't let you do that."
+
+/* (62) Is it a vowel on this position?
+Check if it is a vowel(a, e, i, o, u,) on the n position in a string (the first argument).
+Don't forget about uppercase.
+
+A few cases:
+{
+checkVowel('cat', 1)  ->   true // 'a' is a vowel
+checkVowel('cat', 0)  ->   false // 'c' is not a vowel
+checkVowel('cat', 4)  ->   false // this position doesn't exist
+}
+P.S. If n < 0, return false
+
+*/
+function checkVowel(string, position) {
+  return string.charAt(position).match(/[aeiou]/gi) ? true : false;
+  // [OR]
+  // return /[aeiou]/i.test(string[position] || "");
+}
+console.log(checkVowel("cat", 1)); // true
+console.log(checkVowel("cat", 0)); // false
+console.log(checkVowel("Amanda", -2)); // false
