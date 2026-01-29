38a1623b4b30cda05f6c0650909cb066f84eca41
Your Name <ranamostafa2210@gmail.com>
2026-01-29 17:23:01 +0200

    Solve Previous multiple of three problem

--- kata7.js
+++ kata7.js
@@ -1940,6 +1940,31 @@ oddOne([2,4,6,8]) // => -1
 function oddOne(arr) {
   return arr.findIndex((num) => num % 2 !== 0);
 }
-console.log(oddOne([2, 4, 6, 7, 10])); //3
-console.log(oddOne([2, 16, 98, 10, 13, 78])); //4
-console.log(oddOne([2, 4, 6, 8])); //-1
+// console.log(oddOne([2, 4, 6, 7, 10])); //3
+// console.log(oddOne([2, 16, 98, 10, 13, 78])); //4
+// console.log(oddOne([2, 4, 6, 8])); //-1
+
+/* (67) Previous multiple of three
+Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number
+that is a multiple of three.
+
+Return n if the input is already a multiple of three, and if no such number exists, return null,
+a similar empty value, or -1.
+
+Examples
+1      => null
+25     => null
+36     => 36
+1244   => 12
+952406 => 9
+
+*/
+const prevMultOfThree = (n) => {
+  return (
+    (n % 3 === 0 ? n : prevMultOfThree(Number(n.toString().slice(0, -1)))) ||
+    null
+  );
+};
+console.log(prevMultOfThree(1244)); // 12
+console.log(prevMultOfThree(952406)); //9
+console.log(prevMultOfThree(1)); //null
