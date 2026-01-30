46bfd2298b09555ec09b21c310ed1bf66164e608
Your Name <ranamostafa2210@gmail.com>
2026-01-30 23:13:17 +0200

    Solve Find the vowels problem

--- kata7.js
+++ kata7.js
@@ -1987,6 +1987,42 @@ function solve(s) {
     ? s.toUpperCase()
     : s.toLowerCase();
 }
-console.log(solve("CODe")); // "CODE"
-console.log(solve("COde")); // "code"
-console.log(solve("Code")); // "code"
+// console.log(solve("CODe")); // "CODE"
+// console.log(solve("COde")); // "code"
+// console.log(solve("Code")); // "code"
+
+/* (69) Find the vowels
+We want to know the index of the vowels in a given word, for example, there are two vowels 
+in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!)
+
+*/
+function vowelIndices(word) {
+  return Array(word.length)
+    .fill(0)
+    .map((_, i) => ("aeiouy".includes(word[i].toLowerCase()) ? i + 1 : 0))
+    .filter((i) => i !== 0);
+  // [OR]
+  // let result = [];
+  // for (let i = 0; i < word.length; i++) {
+  //   if ("aeiouy".includes(word[i].toLowerCase())) {
+  //     result.push(i + 1);
+  //   }
+  // }
+  // return result;
+}
+console.log(vowelIndices("apple")); // [1, 5]
+console.log(vowelIndices("Super")); // [2, 4]
+console.log(vowelIndices("Mmmm")); // []
+console.log(vowelIndices("YoMama")); // [1, 2, 4, 6]
+console.log(vowelIndices("supercalifragilisticexpialidocious")); //  [2,4,7,9,12,14,16,19,21,24,25,27,29,31,32,33]
