fa755740b36d001c679a84ed510f6097e418be36
Your Name <ranamostafa2210@gmail.com>
2026-01-31 22:21:49 +0200

    Solve Identify Case problem

--- kata7.js
+++ kata7.js
@@ -2021,8 +2021,53 @@ function vowelIndices(word) {
   // }
   // return result;
 }
-console.log(vowelIndices("apple")); // [1, 5]
-console.log(vowelIndices("Super")); // [2, 4]
-console.log(vowelIndices("Mmmm")); // []
-console.log(vowelIndices("YoMama")); // [1, 2, 4, 6]
-console.log(vowelIndices("supercalifragilisticexpialidocious")); //  [2,4,7,9,12,14,16,19,21,24,25,27,29,31,32,33]
+// console.log(vowelIndices("apple")); // [1, 5]
+// console.log(vowelIndices("Super")); // [2, 4]
+// console.log(vowelIndices("Mmmm")); // []
+// console.log(vowelIndices("YoMama")); // [1, 2, 4, 6]
+// console.log(vowelIndices("supercalifragilisticexpialidocious")); //  [2,4,7,9,12,14,16,19,21,24,25,27,29,31,32,33]
+
+/* (70) Identify Case
+We’ve all seen katas that ask for conversion from snake-case to camel-case, from camel-case
+to snake-case, or from camel-case to kebab-case — the possibilities are endless.
+
+But if we don’t know the case our inputs are in, these are not very helpful.
+
+Task:
+So the task here is to implement a function that takes a string and returns a string with
+ the case the input is in. The possible case types are "kebab", "camel", and "snake". If none of the cases match with the input, or if there are no separators / case changes in the input, return "none". Inputs will only have letters (no numbers or special characters).
+
+Some definitions
+Kebab case: lowercase-words-separated-by-hyphens
+
+Camel case: lowercaseFirstWordFollowedByCapitalizedWords
+
+Snake case: lowercase_words_separated_by_underscores
+
+Examples:
+"hello-world" => "kebab"
+"hello-to-the-world" => "kebab"
+"helloWorld" => "camel"
+"helloToTheWorld" => "camel"
+"hello_world" => "snake"
+"hello_to_the_world" => "snake"
+"hello__world" => "none"
+"hello_World" => "none"
+"helloworld" => "none"
+"hello-World" => "none"
+
+*/
+function id(c_str) {
+  return /^[a-z]+(-[a-z]+)*$/.test(c_str)
+    ? "kebab"
+    : /^[a-z]+(_[a-z]+)*$/.test(c_str)
+      ? "snake"
+      : /^[a-z]+([A-Z][a-z]*)*$/.test(c_str)
+        ? "camel"
+        : "none";
+}
+console.log(id("hello-world")); // "kebab"
+console.log(id("hello_to_the_world")); // "snake"
+console.log(id("helloToTheWorld")); // "camel"
+console.log(id("hello-World")); //none
+console.log(id("hello-to-the-world")); //kebab
