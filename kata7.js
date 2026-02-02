d24238e15e55692e17abde8059b06d9e9dcfa15d
Your Name <ranamostafa2210@gmail.com>
2026-02-02 21:08:09 +0200

    Solve Keypad horror problem

--- kata7.js
+++ kata7.js
@@ -2179,4 +2179,46 @@ let counselors = [
   ["Mike", 7],
   ["Alysa", 3],
 ];
-console.log(killcount(counselors, 7)); // ['Alysa']
+// console.log(killcount(counselors, 7)); // ['Alysa']
+
+/* (75) Keypad horror
+Having two standards for a keypad layout is inconvenient!
+Computer keypad's layout:
+7 8 9  \n
+4 5 6  \n
+1 2 3  \n
+  0 \n
+
+Cell phone keypad's layout:
+1 2 3\n 
+4 5 6\n  
+7 8 9\n  
+  0\n
+
+Solve the horror of unstandardized keypads by providing a function that converts computer input to
+a number as if it was typed on a phone.
+
+Example:
+"789" -> "123"
+
+Notes:
+You get a string with numbers only
+
+*/
+function computerToPhone(numbers) {
+  const nums = {
+    7: "1",
+    8: "2",
+    9: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    1: "7",
+    2: "8",
+    3: "9",
+    0: "0",
+  };
+  return [...numbers].map((digit) => nums[digit]).join("");
+}
+console.log(computerToPhone("789")); // "123"
+console.log(computerToPhone("0789456123")); // "0123456789"
