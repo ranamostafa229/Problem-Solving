66f5573ae43a48e16fdb807e220eaf93f7e2950c
Your Name <ranamostafa2210@gmail.com>
2026-02-03 21:46:47 +0200

    Solve Product of Largest Pair problem

--- kata7.js
+++ kata7.js
@@ -2220,5 +2220,28 @@ function computerToPhone(numbers) {
   };
   return [...numbers].map((digit) => nums[digit]).join("");
 }
-console.log(computerToPhone("789")); // "123"
-console.log(computerToPhone("0789456123")); // "0123456789"
+// console.log(computerToPhone("789")); // "123"
+// console.log(computerToPhone("0789456123")); // "0123456789"
+
+/* (76) Product of Largest Pair
+Rick wants a faster way to get the product of the largest pair in an array. 
+Your task is to create a performant solution to find the product of the largest two integers 
+in a unique array of positive numbers.
+All inputs will be valid.
+Passing [2, 6, 3] should return 18, the product of [6, 3].
+
+Disclaimer: only accepts solutions that are faster than his, which has a running time O(nlogn).
+
+maxProduct([2, 1, 5, 0, 4, 3])              // 20
+maxProduct([7, 8, 9])                       // 72
+maxProduct([33, 231, 454, 11, 9, 99, 57])   // 104874
+
+*/
+function maxProduct(a) {
+  const max = Math.max(...a);
+  const maxIndex = a.indexOf(max);
+  a.splice(maxIndex, 1); // delete max from array
+  return max * Math.max(...a);
+}
+console.log(maxProduct([56, 335, 195, 443, 6, 494, 252])); // 218842
+console.log(maxProduct([2, 1, 5, 0, 4, 3])); // 20
