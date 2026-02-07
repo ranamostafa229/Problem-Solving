d6f2198573859c100499b5ddec2cd1c58579912e
Your Name <ranamostafa2210@gmail.com>
2026-02-07 23:46:54 +0200

    Solve Sum even numbers problem

--- kata7.js
+++ kata7.js
@@ -2363,6 +2363,24 @@ filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 function filter_list(l) {
   return l.filter((e) => typeof e === "number");
 }
-console.log(filter_list([1, 2, "a", "b"])); // [1,2]
-console.log(filter_list([1, "a", "b", 0, 15])); // [1,0,15]
-console.log(filter_list([1, 2, "aasf", "1", "123", 123])); // [1,2,123]
+// console.log(filter_list([1, 2, "a", "b"])); // [1,2]
+// console.log(filter_list([1, "a", "b", 0, 15])); // [1,0,15]
+// console.log(filter_list([1, 2, "aasf", "1", "123", 123])); // [1,2,123]
+
+/* (82) Sum even numbers
+Complete the function that takes a sequence of numbers as single parameter.
+Your function must return the sum of the even values of this sequence.
+
+Only numbers without decimals like 4 or 4.0 can be even.
+
+The input is a sequence of numbers: integers and/or floats.
+
+Examples
+[4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
+[]                               -->  0
+
+*/
+function sumEvenNumbers(input) {
+  return input.filter((n) => n % 2 === 0).reduce((acc, cur) => acc + cur, 0);
+}
+console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); //30
