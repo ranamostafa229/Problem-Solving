467e233e51513ceca08a2e6bcd837d1f276a69b1
Your Name <ranamostafa2210@gmail.com>
2026-02-07 23:41:54 +0200

    Solve List Filtering problem

--- kata7.js
+++ kata7.js
@@ -2345,7 +2345,24 @@ function uniqueSum(lst) {
   // [OR]
   // return [...new Set(lst)].reduce((acc, curr) => acc + curr, null);
 }
-console.log(uniqueSum([1, 2, 3])); //6
-console.log(uniqueSum([1, 3, 8, 1, 8])); //12
-console.log(uniqueSum([])); // null
-console.log(uniqueSum([-1, -1, 5, 2, -7])); //-1
+// console.log(uniqueSum([1, 2, 3])); //6
+// console.log(uniqueSum([1, 3, 8, 1, 8])); //12
+// console.log(uniqueSum([])); // null
+// console.log(uniqueSum([-1, -1, 5, 2, -7])); //-1
+
+/* (81) List Filtering
+In this kata you will create a function that takes a list of non-negative integers 
+and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+*/
+function filter_list(l) {
+  return l.filter((e) => typeof e === "number");
+}
+console.log(filter_list([1, 2, "a", "b"])); // [1,2]
+console.log(filter_list([1, "a", "b", 0, 15])); // [1,0,15]
+console.log(filter_list([1, 2, "aasf", "1", "123", 123])); // [1,2,123]
