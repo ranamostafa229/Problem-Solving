ecf984d1275e6d5457631c0f238fdf0febd75cf0
Your Name <ranamostafa2210@gmail.com>
2026-02-08 22:58:34 +0200

    Solve Sort by Last Char problem

--- kata7.js
+++ kata7.js
@@ -2407,5 +2407,24 @@ function rowSumOddNumbers(n) {
   // [OR]
   // return Math.pow(n, 3);
 }
-console.log(rowSumOddNumbers(42)); // 74088
-console.log(rowSumOddNumbers(1)); // 1
+// console.log(rowSumOddNumbers(42)); // 74088
+// console.log(rowSumOddNumbers(1)); // 1
+
+/* (84) Sort by Last Char
+Given a string of words (x), you need to return an array of the words,
+sorted alphabetically by the final character in each.
+
+If two words have the same last letter, the returned array should show them 
+in the order they appeared in the given string.
+
+All inputs will be valid.
+
+*/
+function last(x) {
+  return x
+    .split(" ")
+    .sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+  // return x.split(" ").sort((a, b) => a.slice(-1).localeCompare(b.slice(-1)));
+}
+
+console.log(last("man i need a taxi up to ubud")); // ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
