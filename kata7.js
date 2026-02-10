d2353cabdeab198061ca50325b353b18bba5064a
Your Name <ranamostafa2210@gmail.com>
2026-02-11 00:04:49 +0200

    Solve Playing with Sets : Equal or Not ? problem

--- kata7.js
+++ kata7.js
@@ -2518,11 +2518,72 @@ C = union(A,B) // -> {1,2,3}
 function union(s1, s2) {
   return new Set([...s1, ...s2]);
 }
-const A = new Set([1, 2]),
-  B = new Set([2, 3]),
-  C = new Set([1, 2, 3]);
-const EMPTY = new Set();
+// const A = new Set([1, 2]),
+// B = new Set([2, 3]),
+// C = new Set([1, 2, 3]);
+// const EMPTY = new Set();
 
-console.log(union(A, B)); // C = {1, 2, 3}
-console.log(union(A, EMPTY)); // A = {1, 2}
-console.log(union(EMPTY, EMPTY)); // EMPTY = {}
+// console.log(union(A, B)); // C = {1, 2, 3}
+// console.log(union(A, EMPTY)); // A = {1, 2}
+// console.log(union(EMPTY, EMPTY)); // EMPTY = {}
+
+/* (89) Playing with Sets : Equal or Not ?
+Set objects are new JavaScript built-in objects defined since ECMAScript 2015
+
+A Set lets you store unique values of any type. It comes with some useful methods like
+.add, .clear, .has . . . BUT some "Set operations" are missing, like . . .
+
+Equality
+
+Two sets ( A, B ) are considered "equal" if all elements of A are elements of B and all elements
+of B are elements of A no matter "order" of elements.
+
+Examples:
+  {1, 2} == {2, 1}
+  {1, 2} != {1, 2, 3}
+Task
+Create 2 functions, areEqual and notEqual, getting 2 sets as arguments and returning a true or false
+depending if these 2 sets are "equal" (respectively not equal) ie : 
+if all elements of 1st set are elements of 2d and all elements of 2d set are elements of 1st.
+
+Examples:
+A  = new Set([1,2]);
+A2 = new Set([2,1]);
+B  = new Set([2,3]);
+
+areEqual(A,B)  // -> false
+areEqual(A,A2) // -> true
+notEqual(A,B)  // -> true
+
+
+*/
+
+function areEqual(s1, s2) {
+  return (
+    [...s1].every((e) => [...s2].includes(e)) &&
+    [...s2].every((e) => [...s1].includes(e))
+  );
+  // [OR]
+  // if (s1.size !== s2.size) return false;
+  // for (let e of s1) {
+  //   if (!s2.has(e)) return false;
+  // }
+  // return true;
+}
+
+function notEqual(s1, s2) {
+  return !areEqual(s1, s2);
+}
+const A = new Set([1, 2]);
+A2 = new Set([2, 1]);
+B = new Set([2, 3]);
+
+console.log(areEqual(A, B)); // false
+console.log(areEqual(A, A2)); // true
+console.log(notEqual(A, B)); // true
+console.log(
+  notEqual(
+    new Set([1, 2, 3, 4, 5]),
+    new Set([1, 2, 3, 4, 5, 6, "z", "x", 111]),
+  ),
+); // false
