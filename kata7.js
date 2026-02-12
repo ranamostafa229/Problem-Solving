d423d95dc7fbb1ffb74d6132d8639aec9398d7dc
Your Name <ranamostafa2210@gmail.com>
2026-02-12 15:27:41 +0200

    Solve Playing with Sets : Intersection problem

--- kata7.js
+++ kata7.js
@@ -2574,16 +2574,49 @@ function areEqual(s1, s2) {
 function notEqual(s1, s2) {
   return !areEqual(s1, s2);
 }
-const A = new Set([1, 2]);
-A2 = new Set([2, 1]);
-B = new Set([2, 3]);
+// const A = new Set([1, 2]);
+// A2 = new Set([2, 1]);
+// B = new Set([2, 3]);
 
-console.log(areEqual(A, B)); // false
-console.log(areEqual(A, A2)); // true
-console.log(notEqual(A, B)); // true
-console.log(
-  notEqual(
-    new Set([1, 2, 3, 4, 5]),
-    new Set([1, 2, 3, 4, 5, 6, "z", "x", 111]),
-  ),
-); // false
+// console.log(areEqual(A, B)); // false
+// console.log(areEqual(A, A2)); // true
+// console.log(notEqual(A, B)); // true
+// console.log(
+//   notEqual(
+//     new Set([1, 2, 3, 4, 5]),
+//     new Set([1, 2, 3, 4, 5, 6, "z", "x", 111]),
+//   ),
+// ); // false
+
+/* (90) Playing with Sets : Intersection
+Set objects are new JavaScript built-in objects defined since ECMAScript 2015
+
+A Set lets you store unique values of any type. It comes with some useful methods
+like .add, .clear, .has . . . BUT some "Set operations" are missing, like . . .
+
+Intersection
+
+A new set can be constructed by determining which members two sets have "in common". 
+The intersection of A and B, denoted by A ∩ B, is the set of all things that are members
+of both A and B. If A ∩ B = ∅, then A and B are said to be disjoint.
+
+Examples:
+  {1, 2} ∩ {1, 2} = {1, 2}.
+  {1, 2} ∩ {2, 3} = {2}.
+Task
+Create function inter getting 2 sets as arguments and returning a new Set as result
+of intersection of these 2 sets.
+
+Examples:
+A = new Set([1,2]);
+B = new Set([2,3]);
+
+C = inter(A,B) // -> {2}
+
+
+*/
+function inter(s1, s2) {
+  return new Set([...s1].filter((e) => s2.has(e)));
+}
+console.log(inter(new Set([1, 2]), new Set([1, 2]))); // {1, 2}
+console.log(inter(new Set([1, 2]), new Set([2, 3]))); // {2}
