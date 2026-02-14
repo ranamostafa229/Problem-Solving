69225b25c846d252b1e9a3b0fe1a2222407a02ac
Your Name <ranamostafa2210@gmail.com>
2026-02-14 23:49:54 +0200

    Solve Playing with Sets : Sup/Sub problem

--- kata7.js
+++ kata7.js
@@ -2654,5 +2654,56 @@ function diff(s1, s2) {
   // ...
   return new Set([...s1].filter((e) => !s2.has(e)));
 }
-console.log(diff(new Set([1, 2]), new Set([2, 3]))); // {1}
-console.log(diff(new Set([2, 3]), new Set([1, 2]))); // {3}
+// console.log(diff(new Set([1, 2]), new Set([2, 3]))); // {1}
+// console.log(diff(new Set([2, 3]), new Set([1, 2]))); // {3}
+
+/* (92) Playing with Sets : Sup/Sub
+Set objects are new JavaScript built-in objects defined since ECMAScript 2015
+
+A Set lets you store unique values of any type. It comes with some useful methods
+like .add, .clear, .has . . . BUT some "Set operations" are missing, like . . .
+
+Subset and Superset
+
+If every member of set A is also a member of set B, then A is said to be a subset of B,
+written A ⊆ B (also pronounced "A is contained in B"). Equivalently, we can write B ⊇ A,
+read as "B is a superset of A", "B includes A", or "B contains A".
+
+Example:
+  {1, 3} ⊆ {1, 2, 3, 4}.
+  {1, 2, 3, 4} ⊆ {1, 2, 3, 4}.
+  
+  {1, 2, 3, 4} ⊇ {1, 3}.
+  {1, 2, 3, 4} ⊇ {1, 2, 3, 4}.
+Task
+Create 2 functions:
+
+isSubsetOf getting 2 sets as arguments and returning true if 2d set contains all elements of 1st one.
+
+isSupersetOf getting 2 sets as arguments and returning true if 1st set contains all elements of 2d one.
+
+Examples:
+A = new Set([1,2]);
+B = new Set([1,2,3]);
+
+isSubsetOf(A,B) // -> true
+isSubsetOf(B,A) // -> false
+
+isSupersetOf(A,B) // -> false
+isSupersetOf(B,A) // -> true
+ 
+
+*/
+function isSubsetOf(s1, s2) {
+  return [...s1].every((e) => s2.has(e));
+}
+
+function isSupersetOf(s1, s2) {
+  return [...s2].every((e) => s1.has(e));
+  // [OR]
+  // return isSubsetOf(s2, s1);
+}
+console.log(isSubsetOf(new Set([1, 2]), new Set([1, 2, 3]))); // true
+console.log(isSubsetOf(new Set([1, 2, 3]), new Set([1, 2]))); // false
+console.log(isSupersetOf(new Set([1, 2]), new Set([1, 2, 3]))); // false
+console.log(isSupersetOf(new Set([1, 2, 3]), new Set([1, 2]))); // true
