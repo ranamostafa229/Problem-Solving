48ccc569c1466dd5e8011aac3d2d7f6282115fa7
Your Name <ranamostafa2210@gmail.com>
2026-02-16 22:26:10 +0200

    Solve How much coffee do you need? problem

--- kata7.js
+++ kata7.js
@@ -2771,7 +2771,44 @@ function validParentheses(parenStr) {
     }, 0) === 0
   ); // at the end, we should have an equal number of opening and closing parentheses
 }
-console.log(validParentheses("()")); // true
-console.log(validParentheses(")(()))")); // false
-console.log(validParentheses("(")); // false
-console.log(validParentheses("(())((()())())")); // true
+// console.log(validParentheses("()")); // true
+// console.log(validParentheses(")(()))")); // false
+// console.log(validParentheses("(")); // false
+// console.log(validParentheses("(())((()())())")); // true
+
+/* (95) How much coffee do you need?
+Everybody know that you passed to much time awake during night time...
+
+Your task here is to define how much coffee you need to stay awake after your night.
+You will have to complete a function that take an array of events in arguments, according
+to this list you will return the number of coffee you need to stay awake during day time.
+Note: If the count exceed 3 please return 'You need extra sleep'.
+
+The list of events can contain the following:
+
+You come here, to solve some kata ('cw').
+You have a dog or a cat that just decide to wake up too early ('dog' | 'cat').
+You just watch a movie ('movie').
+Other events can be present and it will be represent by arbitrary string, just ignore this one.
+Each event can be downcase/lowercase, or uppercase. If it is downcase/lowercase you need 1 coffee
+by events and if it is uppercase you need 2 coffees.
+
+*/
+function howMuchCoffee(events) {
+  const event = ["cw", "dog", "cat", "movie"];
+
+  count = events.reduce(
+    (acc, cur) =>
+      event.includes(cur.toLowerCase())
+        ? acc + (cur === cur.toLowerCase() ? 1 : 2)
+        : acc,
+    0,
+  );
+  return count > 3 ? "You need extra sleep" : count;
+}
+console.log(howMuchCoffee([])); // 0
+console.log(howMuchCoffee(["cw"])); // 1
+console.log(howMuchCoffee(["CW"])); // 2
+console.log(howMuchCoffee(["cw", "CAT"])); // 3
+console.log(howMuchCoffee(["cw", "CAT", "DOG"])); // 'You need extra sleep')
+console.log(howMuchCoffee(["cw", "CAT", "cw=others"])); // 3
