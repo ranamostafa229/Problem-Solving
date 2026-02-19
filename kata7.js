1c30f0b75d7fea6f8aa07c8ecff019b58df80535
Your Name <ranamostafa2210@gmail.com>
2026-02-19 23:34:22 +0200

    Solve Alternate Square Sum problem

--- kata7.js
+++ kata7.js
@@ -2827,6 +2827,44 @@ Examples (input -> output):
 Array.prototype.remove_ = function (integer_list, values_list) {
   return integer_list.filter((num) => !values_list.includes(num));
 };
-console.log(
-  [].remove_([1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2]),
-); // [5, 6, 7, 8]
+// console.log(
+//   [].remove_([1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2]),
+// ); // [5, 6, 7, 8]
+
+/* (97) Alternate Square Sum
+Complete the function that takes an array of integers as input and finds the sum of squares 
+of the elements at even positions (i.e. 2nd, 4th, etc.) plus the sum of the rest of the elements
+at odd position.
+
+For empty arrays, result should be zero (except for Haskell).
+
+Note
+The values at even positions need to be squared. For a language with zero-based indices,
+this will occur at oddly-indexed locations. For instance, in Python, the values at indices 1, 3, 5, etc. should be squared because these are the second, fourth, and sixth positions in the list.
+
+Example
+[11, 12, 13, 14, 15]  -->  379
+# 1.  2.  3.  4.  5.  position
+
+11 + 12^2 + 13 + 14^2 + 15 = 379
+Explanation:
+
+Elements at indices 0, 2, 4 are 11, 13, 15 and they are at odd positions as 11 is at position #1,
+13 is at position #3 and 15 at #5.
+
+Elements at indices 1, 3 are 12 and 14 and they are at even position.
+So we need to add 11, 13, 15 as they are and square of 12 and 14
+
+*/
+function alternateSqSum(arr) {
+  // let sum = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //   sum += i % 2 !== 0 ? arr[i] ** 2 : arr[i];
+  // }
+  // return sum;
+  // [or]
+  return arr.reduce((acc, cur, i) => acc + (i % 2 !== 0 ? cur ** 2 : cur), 0);
+}
+console.log(alternateSqSum([])); // 0
+console.log(alternateSqSum([1, 2, 3, 4, 5])); // 29
+console.log(alternateSqSum([-1, 0, -3, 0, -5, 3])); // 0
