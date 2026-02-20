270e3a4aae3e3e5cae9bacab1bb65d38cfd39690
Your Name <ranamostafa2210@gmail.com>
2026-02-20 22:44:08 +0200

    Solve Alphabetize a list by the nth character problem

--- kata7.js
+++ kata7.js
@@ -2911,7 +2911,46 @@ function reverseInvert(array) {
       return num < 0 ? revertedNum : -revertedNum;
     });
 }
-console.log(reverseInvert([1, 2, 3, 4, 5])); // [-1,-2,-3,-4,-5]
-console.log(reverseInvert([-10])); // [1]
-console.log(reverseInvert([-9, -18, 99])); // [9,81,-99]
-console.log(reverseInvert([1, 12, "a", 3.4, 87, 99.9, -42, 50, 5.6])); //[-1,-21,-78,24,-5]
+// console.log(reverseInvert([1, 2, 3, 4, 5])); // [-1,-2,-3,-4,-5]
+// console.log(reverseInvert([-10])); // [1]
+// console.log(reverseInvert([-9, -18, 99])); // [9,81,-99]
+// console.log(reverseInvert([1, 12, "a", 3.4, 87, 99.9, -42, 50, 5.6])); //[-1,-21,-78,24,-5]
+
+/* (100) Alphabetize a list by the nth character
+Write a function that accepts two parameters, i) a string (containing a list of words) and ii)
+an integer (n). The function should alphabetize the list based on the nth letter of each word.
+
+The letters should be compared case-insensitive. 
+If both letters are the same, order them normally (lexicographically), again, case-insensitive.
+
+Example:
+
+function sortIt('bid, zag', 2) //=> 'zag, bid'
+The length of all words provided in the list will be >= n. The format will be "x, x, x".
+In Haskell you'll get a list of Strings instead.
+
+*/
+function sortIt(list, n) {
+  return list
+    .split(", ")
+    .sort((a, b) => {
+      const nthComparison = a
+        .toLowerCase()
+        [n - 1].localeCompare(b.toLowerCase()[n - 1]);
+      return nthComparison !== 0 // if the nth letter is the same => 0
+        ? nthComparison
+        : a.localeCompare(b);
+    })
+    .join(", ");
+  //[OR]
+  // return list
+  //   .split(", ")
+  //   .sort(
+  //     (a, b) =>
+  //       a[n - 1].toLowerCase().localeCompare(b[n - 1].toLowerCase()) ||
+  //       a.localeCompare(b),
+  //   )
+  //   .join(", ");
+}
+console.log(sortIt("bill, bell, ball, bull", 2)); // 'ball, bell, bill, bull'
+console.log(sortIt("cat, dog, eel, bee", 3)); // 'bee, dog, eel, cat'
