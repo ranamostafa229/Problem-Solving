5eb566f4c29f48bb6ad3e68bbd047524b3d7f790
Your Name <ranamostafa2210@gmail.com>
2026-02-22 00:00:58 +0200

    Solve Array Info problem

--- kata7.js
+++ kata7.js
@@ -2952,5 +2952,56 @@ function sortIt(list, n) {
   //   )
   //   .join(", ");
 }
-console.log(sortIt("bill, bell, ball, bull", 2)); // 'ball, bell, bill, bull'
-console.log(sortIt("cat, dog, eel, bee", 3)); // 'bee, dog, eel, cat'
+// console.log(sortIt("bill, bell, ball, bull", 2)); // 'ball, bell, bill, bull'
+// console.log(sortIt("cat, dog, eel, bee", 3)); // 'bee, dog, eel, cat'
+
+/* (101) Array Info
+Brief
+Sometimes we need information about the list/arrays we're dealing with. You'll have to write such
+a function in this kata. Your function must provide the following informations:
+
+Length of the array
+Number of integer items in the array
+Number of float items in the array
+Number of string character items in the array
+Number of whitespace items in the array
+The informations will be supplied in arrays that are items of another array. Like below:
+
+Output array = [[array length],[no of integer items],[no of float items],[no of string chars items],
+[no of whitespace items]]
+
+Added Difficulty
+If any item count in the array is zero, you'll have to replace it with a None/nil/null value
+(according to the language). And of course, if the array is empty then return 'Nothing in the array!.
+For the sake of simplicity, let's just suppose that there are no nested structures.
+Output ====== If you're head is spinning (just kidding!) then these examples will help you out-
+
+   
+array_info([1,2,3.33,4,5.01,'bass','kick',' '])--------->[[8],[3],[2],[2],[1]]    
+array_info([0.001,2,' '])------------------------------>[[3],[1],[1],[None],[1]]   
+array_info([])----------------------------------------->'Nothing in the array!'
+array_info([' '])-------------------------------------->[[1],[None],[None],[None],[1]]
+  
+Remarks
+The input will always be arrays/lists. So no need to check the inputs.
+Hint ==== See the tags!!!
+Now let's get going !
+
+
+Output array = [[array length],[no of integer items],[no of float items],[no of string chars items],
+[no of whitespace items]]
+*/
+function arrayInfo(arr) {
+  if (arr.length === 0) return "Nothing in the array!";
+  return [
+    [arr.length],
+    [arr.filter((x) => x === parseInt(x)).length || null],
+    [arr.filter((x) => typeof x === "number" && /\./.test(x)).length || null],
+    [arr.filter((x) => typeof x === "string" && x !== " ").length || null],
+    [arr.filter((x) => x === " ").length || null],
+  ];
+}
+console.log(arrayInfo([1, 2, 3.33, 4, 5.01, "bass", "kick", " "])); // [[8],[3],[2],[2],[1]]
+console.log(arrayInfo([0.001, 2, " "])); // [[3],[1],[1],[null],[1]]
+console.log(arrayInfo([])); // 'Nothing in the array!'
+console.log(arrayInfo([" "])); // [[1],[null],[null],[null],[1]]
