537e3d515f89190ca1f4fd85b520f8a18b866e72
Your Name <ranamostafa2210@gmail.com>
2026-02-25 23:58:17 +0200

    Solve What dominates your array? problem

--- kata7.js
+++ kata7.js
@@ -3068,6 +3068,30 @@ function remove(str, what) {
   //  [OR]
   // return str.replace(/[a-z]/g, (char) => (what[char]-- > 0 ? "" : char));
 }
-console.log(remove("this is a string", { t: 1, i: 2 })); // 'hs s a string'
-console.log(remove("hello world", { x: 5, i: 2 })); // 'hello world'
-console.log(remove("apples and bananas", { a: 50, n: 1 })); // 'pples d bnns'
+// console.log(remove("this is a string", { t: 1, i: 2 })); // 'hs s a string'
+// console.log(remove("hello world", { x: 5, i: 2 })); // 'hello world'
+// console.log(remove("apples and bananas", { a: 50, n: 1 })); // 'pples d bnns'
+
+/* (104) What dominates your array?
+A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value
+that occurs in more than half of the elements of arr.
+For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers,
+returns the dominator of arr.
+The function should return −1 if array does not have a dominator. All values in arr will be >=0.
+*/
+function dominator(arr) {
+  const counts = arr.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1; // count the occurrences of each number in the array
+    return acc;
+  }, {});
+  const half = arr.length / 2;
+  for (const [key, value] of Object.entries(counts)) {
+    if (value > half) return parseInt(key);
+  }
+  return -1; // if no dominator is found
+}
+console.log(dominator([3, 4, 3, 2, 3, 1, 3, 3])); // 3
+console.log(dominator([1, 2, 3, 4])); // -1
+console.log(dominator([1, 1, 1, 2, 2, 2, 2])); // 2
