20377e2c4d119a262edd90a07571e50537be3919
Your Name <ranamostafa2210@gmail.com>
2026-03-04 23:33:02 +0200

    Solve Search for letters problem

--- kata7.js
+++ kata7.js
@@ -3216,8 +3216,35 @@ const powers = (n) => {
     .map((str, idx) => (+str ? 2 ** idx : 0)) // if the bit is 1, return the corresponding power of 2, otherwise return 0
     .filter(Boolean); // remove all falsy values => 0
 };
-console.log(powers(6)); // [2, 4]
-console.log(powers(8)); // [8]
-console.log(powers(13)); // [1,4,8]
-console.log(powers(2)); // [2]
-console.log(powers(1)); // [1]
+// console.log(powers(6)); // [2, 4]
+// console.log(powers(8)); // [8]
+// console.log(powers(13)); // [1,4,8]
+// console.log(powers(2)); // [2]
+// console.log(powers(1)); // [1]
+
+/* (108) Search for letters
+Create a function which accepts one arbitrary string as an argument, and return a string of length 26.
+
+The objective is to set each of the 26 characters of the output string to either '1' or '0' based on
+the fact whether the Nth letter of the alphabet is present in the input (independent of its case).
+
+So if an 'a' or an 'A' appears anywhere in the input string (any number of times), set the first character
+of the output string to '1', otherwise to '0'. if 'b' or 'B' appears in the string,
+set the second character to '1', and so on for the rest of the alphabet.
+
+For instance:
+
+"a   **&  cZ"  =>  "10100000000000000000000001"
+"aaaaaaa79345675"  =>  "10000000000000000000000000"
+"&%#*"  =>  "00000000000000000000000000"
+
+
+*/
+function change(string) {
+  return [..."abcdefghijklmnopqrstuvwxyz"].reduce((acc, char) => {
+    return acc + (string.toLowerCase().includes(char) ? "1" : "0");
+  }, "");
+}
+console.log(change("a **&  bZ")); // "11000000000000000000000001"
+console.log(change("aaaaaaa79345675")); //  "10000000000000000000000000"
+console.log(change("&%#*")); // "00000000000000000000000000"
