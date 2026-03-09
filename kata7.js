63a24e1dd3cb0c5b6122bc8c0eeec5a9c391dbe3
Your Name <ranamostafa2210@gmail.com>
2026-03-09 23:41:28 +0200

    Solve Maximum Product probelm

--- kata7.js
+++ kata7.js
@@ -3337,6 +3337,53 @@ function flyBy(lamps, drone) {
   // [OR]
   // return [...lamps].fill("o", 0, drone.length).join("");
 }
-console.log(flyBy("xxxxxx", "====T")); //  'ooooox'
-console.log(flyBy("xxxxxxxxx", "==T")); //  'oooxxxxxx'
-console.log(flyBy("xxxxxxxxxxxxxxx", "=========T")); // 'ooooooooooxxxxx'
+// console.log(flyBy("xxxxxx", "====T")); //  'ooooox'
+// console.log(flyBy("xxxxxxxxx", "==T")); //  'oooxxxxxx'
+// console.log(flyBy("xxxxxxxxxxxxxxx", "=========T")); // 'ooooooooooxxxxx'
+
+/* (111) Maximum Product
+Task
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers
+in the array. Note that the array size is at least 2 and consists a mixture of positive, 
+negative integers and also zeroes.
+
+Examples
+[1, 2, 3] returns 6 because the maximum product is obtained from multiplying 
+ 
+2
+∗
+3
+=
+6
+ 2∗3=6
+[9, 5, 10, 2, 24, -1, -48] returns 50 because the maximum product is obtained from multiplying 
+ 
+5
+∗
+10
+=
+50
+ 5∗10=50
+[-23, 4, -5, 99, -27, 329, -2, 7, -921] returns -14 because the maximum product is obtained from multiplying 
+ 
+−
+2
+∗
+7
+=
+−
+14
+ −2∗7=−14
+
+
+*/
+function adjacentElementsProduct(array) {
+  let products = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    products.push(array[i] * array[i + 1]);
+  }
+  return Math.max(...products);
+}
+console.log(adjacentElementsProduct([5, 8])); // 40
+console.log(adjacentElementsProduct([1, 2, 3])); // 6
+console.log(adjacentElementsProduct([1, 5, 10, 9])); //90
