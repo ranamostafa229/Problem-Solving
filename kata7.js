c5553fe929d7ab1637bc6f13cbeae26d6110e9c0
Your Name <ranamostafa2210@gmail.com>
2026-03-10 23:54:40 +0200

    Solve Array element parity problem

--- kata7.js
+++ kata7.js
@@ -3350,29 +3350,14 @@ negative integers and also zeroes.
 Examples
 [1, 2, 3] returns 6 because the maximum product is obtained from multiplying 
  
-2
-∗
-3
-=
-6
- 2∗3=6
+2∗3=6
+
 [9, 5, 10, 2, 24, -1, -48] returns 50 because the maximum product is obtained from multiplying 
  
-5
-∗
-10
-=
-50
- 5∗10=50
+5∗10=50
+ 
 [-23, 4, -5, 99, -27, 329, -2, 7, -921] returns -14 because the maximum product is obtained from multiplying 
  
-−
-2
-∗
-7
-=
-−
-14
  −2∗7=−14
 
 
@@ -3384,6 +3369,35 @@ function adjacentElementsProduct(array) {
   }
   return Math.max(...products);
 }
-console.log(adjacentElementsProduct([5, 8])); // 40
-console.log(adjacentElementsProduct([1, 2, 3])); // 6
-console.log(adjacentElementsProduct([1, 5, 10, 9])); //90
+// console.log(adjacentElementsProduct([5, 8])); // 40
+// console.log(adjacentElementsProduct([1, 2, 3])); // 6
+// console.log(adjacentElementsProduct([1, 5, 10, 9])); //90
+
+/* (112) Array element parity
+In this Kata, you will be given an array of integers whose elements have both a negative and
+a positive value, except for one integer that is either only negative or only positive.
+Your task will be to find that integer.
+
+Examples:
+
+[1, -1, 2, -2, 3] => 3
+
+3 has no matching negative appearance
+
+[-3, 1, 2, 3, -1, -4, -2] => -4
+
+-4 has no matching positive appearance
+
+[1, -1, 2, -2, 3, 3] => 3
+
+(the only-positive or only-negative integer may appear more than once)
+
+
+*/
+function solve(arr) {
+  return arr.find((num) => !arr.includes(-num));
+}
+console.log(solve([1, -1, 2, -2, 3])); // 3
+console.log(solve([-3, 1, 2, 3, -1, -4, -2])); // -4
+console.log(solve([1, -1, 2, -2, 3, 3])); // 3
+console.log(solve([-9, -105, -9, -9, -9, -9, 105])); // -9
