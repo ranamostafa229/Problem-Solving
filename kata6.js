8d3b495eacbe0b45144ab623ec238db9d33cff62
Your Name <ranamostafa2210@gmail.com>
2026-03-19 23:43:14 +0200

    Solve Multiplication table probelm

---
+++ kata6.js
@@ -0,0 +1,21 @@
+/* (1) Multiplication table
+Your task, is to create N×N multiplication table, of size provided in parameter.
+
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+For the given example, the return value should be:
+
+[[1,2,3],[2,4,6],[3,6,9]]
+
+*/
+function multiplicationTable(size) {
+  return [...Array(size)].map((_, i) =>
+    [...Array(size)].map((_, j) => (i + 1) * (j + 1)),
+  );
+}
+console.log(multiplicationTable(3)); // [[1,2,3],[2,4,6],[3,6,9]]
+console.log(multiplicationTable(1)); // [[1]]
+console.log(multiplicationTable(2)); // [[1,2],[2,4]]
