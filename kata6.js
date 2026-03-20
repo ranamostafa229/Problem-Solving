3a6270aa85d97f2384c80bcbd836aa119e0f7f2b
Your Name <ranamostafa2210@gmail.com>
2026-03-20 23:28:01 +0200

    Solution NO.2 for Multiplication table problem

--- kata6.js
+++ kata6.js
@@ -15,6 +15,16 @@ function multiplicationTable(size) {
   return [...Array(size)].map((_, i) =>
     [...Array(size)].map((_, j) => (i + 1) * (j + 1)),
   );
+  // [OR]
+  // let table = [];
+  // for (let i = 0; i < size; i++) {
+  //   let row = [];
+  //   for (let j = 0; j < size; j++) {
+  //     row.push((i + 1) * (j + 1));
+  //   }
+  //   table.push(row);
+  // }
+  // return table;
 }
 console.log(multiplicationTable(3)); // [[1,2,3],[2,4,6],[3,6,9]]
 console.log(multiplicationTable(1)); // [[1]]
