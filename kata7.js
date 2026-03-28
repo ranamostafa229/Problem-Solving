1ca241614caf9379ddb861d537a1fa5190ef969b
Your Name <ranamostafa2210@gmail.com>
2026-03-28 23:58:01 +0200

    Solve Count the Digit problem

--- kata7.js
+++ kata7.js
@@ -3605,5 +3605,36 @@ longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 function longest(s1, s2) {
   return [...new Set(s1 + s2)].sort().join("");
 }
-console.log(longest("aretheyhere", "yestheyarehere")); //"aehrsty"
-console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); //"abcdefghilnoprstu"
+// console.log(longest("aretheyhere", "yestheyarehere")); //"aehrsty"
+// console.log(longest("loopingisfunbutdangerous", "lessdangerousthancoding")); //"abcdefghilnoprstu"
+
+/* (119) Count the Digit
+Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+Square all numbers k (0 <= k <= n) between 0 and n.
+
+Count the numbers of digits d used in the writing of all the k**2.
+
+Implement the function taking n and d as parameters and returning this count.
+
+Examples:
+n = 10, d = 1 
+the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+The function, when given n = 25 and d = 1 as argument, should return 11 since
+the k*k that contain the digit 1 are:
+1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+So there are 11 digits 1 for the squares of numbers between 0 and 25.
+Note that 121 has twice the digit 1.
+
+*/
+function nbDig(n, d) {
+  return [...Array.from({ length: n + 1 }, (_, i) => i * i)]
+    .join("")
+    .split("")
+    .filter((char) => char == d).length;
+}
+console.log(nbDig(5750, 0)); // 4700
+console.log(nbDig(11011, 2)); // 9481
+console.log(nbDig(10, 1)); // 4
